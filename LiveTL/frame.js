9f73db9d46f83e9efca426362f5efe65af649252
KentoNishi <kento24gs@outlook.com>
2020-11-23 19:03:08 -0800

    Updated formatting

--- LiveTL/frame.js
+++ LiveTL/frame.js
@@ -213,7 +213,7 @@ function runLiveTL() {
                 min-width 0px !important;
             }
             .livetl * {
-                vertical-align: top;
+                vertical-align: baseline;
             }
             .navbar{
                 margin-top: 10px;
@@ -268,13 +268,13 @@ function runLiveTL() {
             .translationText{
                 position: absolute;
                 z-index: -1;
-                top: 25px;
-                height: calc(100% - 25px);
-                padding-top: 10px;
+                height: calc(100%);
+                margin-top: 5px;
+                margin-botton: 5px;
             }
 
-            .authorInfo {
-                font-size: 12px;
+            .authorName {
+                font-size: 15px;
                 color: #bdbdbd;
                 margin-left: 5px;
                 vertical-align: baseline;
@@ -286,6 +286,22 @@ function runLiveTL() {
                 -moz-user-select: -moz-none;
                 -o-user-select: none;
                 user-select: none;
+            }
+
+            .hide {
+                stroke-width: 0;
+                fill: cornflowerblue;
+                stroke: rgb(0, 153, 255);
+                width: 15px;
+                height: 15px;
+            }
+
+            .ban {
+                stroke-width: 0;
+                fill: crimson;
+                stroke: rgb(0, 153, 255);
+                width: 15px;
+                height: 15px;
             }
         ` + modalCSS;
 
@@ -407,9 +423,52 @@ function runLiveTL() {
                     line.style.marginTop = "10px";
                     line.textContent = parsed[2];
                     var authorInfo = document.createElement("span");
-                    authorInfo.className = "authorInfo";
+                    var authorName = document.createElement("span");
+                    authorName.className = "authorName";
                     line.appendChild(authorInfo);
-                    authorInfo.textContent = author;
+                    authorName.textContent = author;
+                    authorInfo.appendChild(authorName);
+                    var hide = document.createElement("span");
+                    hide.innerHTML = `
+                    <svg class="hide" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                        viewBox="0 0 572.098 572.098" style="enable-background:new 0 0 572.098 572.098;"
+                        xml:space="preserve">
+                        <g>
+                            <path d="M99.187,398.999l44.333-44.332c-24.89-15.037-47.503-33.984-66.763-56.379c29.187-33.941,66.053-60.018,106.947-76.426
+                                c-6.279,14.002-9.853,29.486-9.853,45.827c0,16.597,3.696,32.3,10.165,46.476l35.802-35.797
+                                c-5.698-5.594-9.248-13.36-9.248-21.977c0-17.02,13.801-30.82,30.82-30.82c8.611,0,16.383,3.55,21.971,9.248l32.534-32.534
+                                l36.635-36.628l18.366-18.373c-21.206-4.186-42.896-6.469-64.848-6.469c-107.663,0-209.732,52.155-273.038,139.518L0,298.288
+                                l13.011,17.957C36.83,349.116,66.151,376.999,99.187,398.999z"/>
+                            <path d="M459.208,188.998l-44.854,44.854c30.539,16.071,58.115,37.846,80.986,64.437
+                                c-52.167,60.662-128.826,96.273-209.292,96.273c-10.3,0-20.533-0.6-30.661-1.744l-52.375,52.375
+                                c26.903,6.887,54.762,10.57,83.036,10.57c107.663,0,209.738-52.154,273.038-139.523l13.011-17.957l-13.011-17.956
+                                C532.023,242.995,497.844,212.15,459.208,188.998z"/>
+                            <path d="M286.049,379.888c61.965,0,112.198-50.234,112.198-112.199c0-5.588-0.545-11.035-1.335-16.402L269.647,378.56
+                                C275.015,379.349,280.461,379.888,286.049,379.888z"/>
+                            <path d="M248.815,373.431L391.79,230.455l4.994-4.994l45.796-45.796l86.764-86.77c13.543-13.543,13.543-35.502,0-49.046
+                                c-6.77-6.769-15.649-10.159-24.523-10.159s-17.754,3.384-24.522,10.159l-108.33,108.336l-22.772,22.772l-29.248,29.248
+                                l-48.14,48.14l-34.456,34.456l-44.027,44.027l-33.115,33.115l-45.056,45.055l-70.208,70.203
+                                c-13.543,13.543-13.543,35.502,0,49.045c6.769,6.77,15.649,10.16,24.523,10.16s17.754-3.385,24.523-10.16l88.899-88.898
+                                l50.086-50.086L248.815,373.431z"/>
+                        </g>
+                    </svg>
+                    `;
+                    authorInfo.appendChild(hide);
+                    var ban = document.createElement("span");
+                    ban.innerHTML = `
+                    <svg class="ban" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <g data-name="Layer 2">
+                            <g data-name="person-delete">
+                                <rect width="24" height="24" opacity="0" />
+                                <path
+                                    d="M20.47 7.5l.73-.73a1 1 0 0 0-1.47-1.47L19 6l-.73-.73a1 1 0 0 0-1.47 1.5l.73.73-.73.73a1 1 0 0 0 1.47 1.47L19 9l.73.73a1 1 0 0 0 1.47-1.5z" />
+                                <path d="M10 11a4 4 0 1 0-4-4 4 4 0 0 0 4 4z" />
+                                <path d="M16 21a1 1 0 0 0 1-1 7 7 0 0 0-14 0 1 1 0 0 0 1 1z" />
+                            </g>
+                        </g>
+                    </svg>
+                    `;
+                    authorInfo.appendChild(ban);
                     if (!(author in allTranslators)) createCheckbox(author, allTranslatorCheckbox.checked);
                     if (allTranslators[author].checked) e.appendChild(line);
                     // line.onmouseover = () => authorInfo.textContent = `Translated by ${author}`;
@@ -507,7 +566,7 @@ function createModal(container) {
 
 const modalCSS = `
 
-#settingsGear {
+svg {
     stroke: #D1D1D1;
 };
 
