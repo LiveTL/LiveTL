3de7e2a30d860d0d085986f68cc37af2d369340b
r2dev2bb8 <ronak.badhe@gmail.com>
2020-11-23 22:21:14 -0800

    modal is now a settings page

--- LiveTL/frame.js
+++ LiveTL/frame.js
@@ -265,6 +265,10 @@ function runLiveTL() {
                 list-style: none;
             }
 
+            .dropdown-check-list {
+                position: absolute;
+            }
+
             .translationText{
                 position: absolute;
                 z-index: -1;
@@ -312,8 +316,6 @@ function runLiveTL() {
         let settings = createModal(livetlContainer);
         let e = document.createElement("div");
         e.className = "translationText";
-        // let eee = document.createElement("img");
-        // eee.src = "https://fonts.gstatic.com/s/i/materialiconsoutlined/settings/v7/24px.svg";
         let select = document.createElement("input");
         select.dataset.role = "none";
         let datalist = document.createElement("datalist");
@@ -333,48 +335,40 @@ function runLiveTL() {
             }
         }
         select.onfocus = () => select.value = "";
-        let navbar = document.createElement("div");
-        // sd.appendChild(select);
-        navbar.className = "navbar";
-        // navbar.appendChild(select);
-        // navbar.appendChild(datalist);
-        // navbar.appendChild(select);
-        // settings svg: "https://fonts.gstatic.com/s/i/materialiconsoutlined/settings/v7/24px.svg";
-        // settings.appendChild(navbar);
-        settings.appendChild(select);
-        settings.appendChild(datalist);
-        livetlContainer.appendChild(e);
-
-        var checklist = document.createElement("div");
+        
+        let checklist = document.createElement("div");
         checklist.className = "dropdown-check-list";
         checklist.tabIndex = 1;
-        // navbar.appendChild(checklist);
-        settings.appendChild(checklist);
-        var defaultText = document.createElement("span");
+        let defaultText = document.createElement("span");
         defaultText.className = "anchor";
         defaultText.textContent = "Select Translators";
         checklist.appendChild(defaultText);
-        var items = document.createElement("ul");
+        let items = document.createElement("ul");
         items.id = "items";
         items.className = "items";
         checklist.appendChild(items);
 
-        var allTranslators = {};
+        settings.appendChild(select);
+        settings.appendChild(datalist);
+        settings.appendChild(checklist);
+        livetlContainer.appendChild(e);
 
-        var allTranslatorCheckbox = {};
+        let allTranslators = {};
+
+        let allTranslatorCheckbox = {};
 
         checkboxUpdate = () => {
             allTranslators = {};
-            var boxes = checklist.querySelectorAll("input");
+            let boxes = checklist.querySelectorAll("input");
             boxes.forEach(box => {
                 allTranslators[box.id] = box;
                 if (box != allTranslatorCheckbox && !box.checked) allTranslatorCheckbox.checked = false;
             });
             if (allTranslatorCheckbox.checked) {
-                var boxes = checklist.querySelectorAll("input:not(:checked)");
+                let boxes = checklist.querySelectorAll("input:not(:checked)");
                 boxes.forEach(box => box.checked = true)
             }
-            var messages = document.querySelectorAll(".line");
+            let messages = document.querySelectorAll(".line");
             for (i = 0; i < messages.length; i++) {
                 if (i < messages.length - 25) {
                     messages[i].remove();
@@ -387,15 +381,15 @@ function runLiveTL() {
         }
 
         createCheckbox = (name, checked = false, callback = null) => {
-            var selectTranslatorMessage = document.createElement("li");
+            let selectTranslatorMessage = document.createElement("li");
             items.append(selectTranslatorMessage);
-            var checkbox = document.createElement("input");
+            let checkbox = document.createElement("input");
             checkbox.type = "checkbox";
             checkbox.id = name;
             checkbox.checked = checked;
             checkbox.onchange = callback || checkboxUpdate;
             selectTranslatorMessage.appendChild(checkbox);
-            var person = document.createElement("label");
+            let person = document.createElement("label");
             person.setAttribute("for", name);
             person.textContent = person.value = name;
             selectTranslatorMessage.appendChild(person);
@@ -403,8 +397,8 @@ function runLiveTL() {
             return checkbox;
         }
 
-        var allTranslatorCheckbox = createCheckbox("All Translators", true, () => {
-            var boxes = checklist.querySelectorAll("input:not(:checked)");
+        allTranslatorCheckbox = createCheckbox("All Translators", true, () => {
+            let boxes = checklist.querySelectorAll("input:not(:checked)");
             boxes.forEach(box => box.checked = allTranslatorCheckbox.checked);
             checkboxUpdate();
         });
@@ -426,19 +420,19 @@ function runLiveTL() {
                 let parsed = /^\[(\w+)\] ?(.+)/.exec(m.textContent);
                 if (parsed != null && parsed[1].toLowerCase() == languageConversionTable[select.value].code) {
                     console.log(parsed);
-                    var author = m.parentElement.childNodes[1].textContent;
+                    let author = m.parentElement.childNodes[1].textContent;
                     let line = document.createElement("div");
                     line.className = "line";
                     line.style.marginBottom = "10px";
                     line.style.marginTop = "10px";
                     line.textContent = parsed[2];
-                    var authorInfo = document.createElement("span");
-                    var authorName = document.createElement("span");
+                    let authorInfo = document.createElement("span");
+                    let authorName = document.createElement("span");
                     authorName.className = "authorName";
                     line.appendChild(authorInfo);
                     authorName.textContent = author;
                     authorInfo.appendChild(authorName);
-                    var hide = document.createElement("span");
+                    let hide = document.createElement("span");
                     hide.style.cursor = "pointer";
                     hide.onclick = () => line.remove();
                     hide.innerHTML = `
@@ -466,7 +460,7 @@ function runLiveTL() {
                     </svg>
                     `;
                     authorInfo.appendChild(hide);
-                    var ban = document.createElement("span");
+                    let ban = document.createElement("span");
                     ban.onclick = () => {
                         allTranslators[author].checked = false;
                         checkboxUpdate();
@@ -525,8 +519,7 @@ window.onload = () => {
                 console.log("Running LiveTL!");
                 runLiveTL();
             }
-        } catch (e) {
-        }
+        } catch (e) { }
     }
 }
 
@@ -549,30 +542,23 @@ function createModal(container) {
     let modalContent = document.createElement("div");
     modalContent.className = "modal-content";
 
-    let closeButton = document.createElement("span");
-    closeButton.className = "close";
-    closeButton.innerHTML = "&times";
+    let nextStyle = {
+        "block": "none",
+        "none": "block",
+    };
+
+    let icon = {
+        "block": closeSVG,
+        "none": settingsGear,
+    };
+
 
     settingsButton.addEventListener("click", (e) => {
-        modalContainer.style.display = "block";
+        let newDisplay = nextStyle[modalContainer.style.display];
+        modalContainer.style.display = newDisplay;
+        settingsButton.innerHTML = icon[newDisplay];
     });
 
-    closeButton.addEventListener("click", (e) => {
-        modalContainer.style.display = "none";
-    });
-
-    container.addEventListener("click", (e) => {
-        let t = e.target;
-        while (t != container && t != null) {
-            if (t == modalContainer || t == settingsButton) {
-                return;
-            }
-            t = t.parentElement;
-        }
-        modalContainer.style.display = "none";
-    });
-
-    modalContent.appendChild(closeButton);
     modalContainer.appendChild(modalContent);
 
     container.appendChild(settingsButton);
@@ -587,43 +573,39 @@ svg {
     stroke: #D1D1D1;
 };
 
+
  /* The Modal (background) */
-.modal {
+.modal, .modal-content {
   display: none; /* Hidden by default */
   position: fixed; /* Stay in place */
   z-index: 1; /* Sit on top */
-  left: 0;
   top: 0;
+  left: 0;
   width: 100%; /* Full width */
   height: 100%; /* Full height */
   overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+  background-color: var(--yt-live-chat-background-color);
+  color: var(--yt-live-chat-primary-text-color);
 }
 
 /* Modal Content/Box */
 .modal-content {
-  background-color: #fefefe;
-  margin: 15% auto; /* 15% from the top and centered */
+  margin: 15% top; /* 15% from the top and centered */
   padding: 20px;
-  border: 1px solid #888;
   width: 80%; /* Could be more or less, depending on screen size */
 }
 
-/* The Close Button */
-.close {
-  color: #aaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
-}
 
-.close:hover,
-.close:focus {
-  color: black;
-  text-decoration: none;
-  cursor: pointer;
-}
+#settingsGear:hover,
+#settingsGear:focus,
+.svgButton:hover,
+.svgButton:focus {
+    stroke: #0099FF1;
+    cursor: pointer;
+};
 `;
 
-const settingsGear = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M19.43 12.98c.04-.32.07-.64.07-.98 0-.34-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.09-.16-.26-.25-.44-.25-.06 0-.12.01-.17.03l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.06-.02-.12-.03-.18-.03-.17 0-.34.09-.43.25l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98 0 .33.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.09.16.26.25.44.25.06 0 .12-.01.17-.03l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.06.02.12.03.18.03.17 0 .34-.09.43-.25l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zm-1.98-1.71c.04.31.05.52.05.73 0 .21-.02.43-.05.73l-.14 1.13.89.7 1.08.84-.7 1.21-1.27-.51-1.04-.42-.9.68c-.43.32-.84.56-1.25.73l-1.06.43-.16 1.13-.2 1.35h-1.4l-.19-1.35-.16-1.13-1.06-.43c-.43-.18-.83-.41-1.23-.71l-.91-.7-1.06.43-1.27.51-.7-1.21 1.08-.84.89-.7-.14-1.13c-.03-.31-.05-.54-.05-.74s.02-.43.05-.73l.14-1.13-.89-.7-1.08-.84.7-1.21 1.27.51 1.04.42.9-.68c.43-.32.84-.56 1.25-.73l1.06-.43.16-1.13.2-1.35h1.39l.19 1.35.16 1.13 1.06.43c.43.18.83.41 1.23.71l.91.7 1.06-.43 1.27-.51.7 1.21-1.07.85-.89.7.14 1.13zM12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 6c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"/></svg>`;
+const closeSVG = `
+<svg class="svgButton" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg>`;
+
+const settingsGear = `<svg class="svgButton" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M19.43 12.98c.04-.32.07-.64.07-.98 0-.34-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.09-.16-.26-.25-.44-.25-.06 0-.12.01-.17.03l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.06-.02-.12-.03-.18-.03-.17 0-.34.09-.43.25l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98 0 .33.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.09.16.26.25.44.25.06 0 .12-.01.17-.03l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.06.02.12.03.18.03.17 0 .34-.09.43-.25l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zm-1.98-1.71c.04.31.05.52.05.73 0 .21-.02.43-.05.73l-.14 1.13.89.7 1.08.84-.7 1.21-1.27-.51-1.04-.42-.9.68c-.43.32-.84.56-1.25.73l-1.06.43-.16 1.13-.2 1.35h-1.4l-.19-1.35-.16-1.13-1.06-.43c-.43-.18-.83-.41-1.23-.71l-.91-.7-1.06.43-1.27.51-.7-1.21 1.08-.84.89-.7-.14-1.13c-.03-.31-.05-.54-.05-.74s.02-.43.05-.73l.14-1.13-.89-.7-1.08-.84.7-1.21 1.27.51 1.04.42.9-.68c.43-.32.84-.56 1.25-.73l1.06-.43.16-1.13.2-1.35h1.39l.19 1.35.16 1.13 1.06.43c.43.18.83.41 1.23.71l.91.7 1.06-.43 1.27-.51.7 1.21-1.07.85-.89.7.14 1.13zM12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 6c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"/></svg>`;
