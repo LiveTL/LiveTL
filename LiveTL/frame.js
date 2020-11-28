ed908f1919bd3fab3259431048776d5799ce258a
r2dev2bb8 <ronak.badhe@gmail.com>
2020-11-27 20:51:13 -0800

    refactor translation adding code

--- LiveTL/frame.js
+++ LiveTL/frame.js
@@ -56,9 +56,8 @@ function runLiveTL() {
             checkboxUpdate();
         });
 
-        prependE = (el) => {
-            translationDiv.prepend(el);
-        }
+        prependE = el => translationDiv.prepend(el);
+
 
         getProfilePic = (el) => {
             try {
@@ -74,112 +73,40 @@ function runLiveTL() {
 
         let firstLoop = true;
         setInterval(() => {
-            // if (select.value in languageConversionTable && select.value != lastLang) translationDiv.innerHTML = "";
-            // let start = (new Date()).getMilliseconds();
             let messages = Array.from(document.querySelectorAll(".yt-live-chat-text-message-renderer > #message"));
             if (firstLoop) {
                 messages = messages.reverse();
                 firstLoop = false;
             }
+
             for (let i = messages.length - 1; i >= 0; i--) {
-                // if (!checkedSet.has(m.textContent)) {
-                //     // console.log(`Scanning message: ${m.textContent}`);
-                //     checkedSet.add(m.textContent);
-                // }
                 let m = messages[i];
-                if (m.innerHTML == "") break;
+                if (m.innerHTML == "") {
+                    break;
+                }
                 let parsed = parseTranslation(m.textContent);
                 let select = document.querySelector("#langSelect");
                 if (parsed != null && parsed.lang.toLowerCase() == languageConversionTable[select.value].code) {
                     console.log(getProfilePic(m));
                     let author = m.parentElement.childNodes[1].textContent;
                     let authorID = getProfilePic(m);
-                    let line = document.createElement("div");
-                    line.className = "line";
-                    line.textContent = parsed.msg;
-                    let authorInfo = document.createElement("span");
-                    let authorName = document.createElement("span");
-                    authorName.className = "authorName";
-                    line.appendChild(authorInfo);
-                    authorName.textContent = author;
-                    authorName.dataset.id = authorID;
-                    authorInfo.appendChild(authorName);
-                    let hide = document.createElement("span");
-                    hide.style.cursor = "pointer";
-                    hide.onclick = () => line.remove();
-                    hide.innerHTML = `
-                    <svg class="hide" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                        viewBox="0 0 572.098 572.098" style="enable-background:new 0 0 572.098 572.098;"
-                        xml:space="preserve">
-                        <g>
-                            <path d="M99.187,398.999l44.333-44.332c-24.89-15.037-47.503-33.984-66.763-56.379c29.187-33.941,66.053-60.018,106.947-76.426
-                                c-6.279,14.002-9.853,29.486-9.853,45.827c0,16.597,3.696,32.3,10.165,46.476l35.802-35.797
-                                c-5.698-5.594-9.248-13.36-9.248-21.977c0-17.02,13.801-30.82,30.82-30.82c8.611,0,16.383,3.55,21.971,9.248l32.534-32.534
-                                l36.635-36.628l18.366-18.373c-21.206-4.186-42.896-6.469-64.848-6.469c-107.663,0-209.732,52.155-273.038,139.518L0,298.288
-                                l13.011,17.957C36.83,349.116,66.151,376.999,99.187,398.999z"/>
-                            <path d="M459.208,188.998l-44.854,44.854c30.539,16.071,58.115,37.846,80.986,64.437
-                                c-52.167,60.662-128.826,96.273-209.292,96.273c-10.3,0-20.533-0.6-30.661-1.744l-52.375,52.375
-                                c26.903,6.887,54.762,10.57,83.036,10.57c107.663,0,209.738-52.154,273.038-139.523l13.011-17.957l-13.011-17.956
-                                C532.023,242.995,497.844,212.15,459.208,188.998z"/>
-                            <path d="M286.049,379.888c61.965,0,112.198-50.234,112.198-112.199c0-5.588-0.545-11.035-1.335-16.402L269.647,378.56
-                                C275.015,379.349,280.461,379.888,286.049,379.888z"/>
-                            <path d="M248.815,373.431L391.79,230.455l4.994-4.994l45.796-45.796l86.764-86.77c13.543-13.543,13.543-35.502,0-49.046
-                                c-6.77-6.769-15.649-10.159-24.523-10.159s-17.754,3.384-24.522,10.159l-108.33,108.336l-22.772,22.772l-29.248,29.248
-                                l-48.14,48.14l-34.456,34.456l-44.027,44.027l-33.115,33.115l-45.056,45.055l-70.208,70.203
-                                c-13.543,13.543-13.543,35.502,0,49.045c6.769,6.77,15.649,10.16,24.523,10.16s17.754-3.385,24.523-10.16l88.899-88.898
-                                l50.086-50.086L248.815,373.431z"/>
-                        </g>
-                    </svg>
-                    `;
-                    let ban = document.createElement("span");
-                    ban.onclick = () => {
-                        allTranslators.v[authorID].checked = false;
-                        checkboxUpdate();
+                    let line = createTranslationElement(author, authorID, parsed.msg);
+                    if (!(authorID in allTranslators.v)) {
+                        createCheckbox(author, authorID, allTranslatorCheckbox.checked);
                     }
-                    ban.style.cursor = "pointer";
-                    ban.innerHTML = `
-                    <svg class="ban" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <g data-name="Layer 2">
-                            <g data-name="person-delete">
-                                <rect width="24" height="24" opacity="0" />
-                                <path
-                                    d="M20.47 7.5l.73-.73a1 1 0 0 0-1.47-1.47L19 6l-.73-.73a1 1 0 0 0-1.47 1.5l.73.73-.73.73a1 1 0 0 0 1.47 1.47L19 9l.73.73a1 1 0 0 0 1.47-1.5z" />
-                                <path d="M10 11a4 4 0 1 0-4-4 4 4 0 0 0 4 4z" />
-                                <path d="M16 21a1 1 0 0 0 1-1 7 7 0 0 0-14 0 1 1 0 0 0 1 1z" />
-                            </g>
-                        </g>
-                    </svg>
-                    `;
-                    let options = document.createElement("span");
-                    options.appendChild(hide);
-                    options.appendChild(ban);
-                    authorInfo.append(options);
-                    if (!(authorID in allTranslators.v)) createCheckbox(author, authorID, allTranslatorCheckbox.checked);
-                    if (allTranslators.v[authorID].checked) prependE(line);
-                    options.style.display = "none";
-                    options.className = "messageOptions";
-                    line.onmouseover = () => options.style.display = "inline-block";
-                    line.onmouseleave = () => options.style.display = "none";
+                    if (allTranslators.v[authorID].checked) {
+                        prependE(line);
+                    }
                 }
-                // m.classList.add("scanned");
-                // m.textContent = "[SCANNED] " + m.textContent;
                 m.innerHTML = "";
             }
+
             let settingsProjection = document.querySelector("#settingsProjection");
             if (settingsProjection) settingsProjection.remove();
             settingsProjection = document.createElement("div");
             settingsProjection.id = "settingsProjection";
             settingsProjection.style.zIndex = -1;
             prependE(settingsProjection);
-            // messages.forEach(m => {
-            //     m.classList.add("scanned");
-            //     let t = document.createElement("span");
-            //     t.textContent = "[SCANNED] ";
-            //     m.prepend(t);
-            // })
-            // if (select.value in languageConversionTable) lastLang = select.value;
-            // console.log(`Scanning took ${(new Date()).getMilliseconds() - start}ms`);
-            // translationDiv.scrollTop = translationDiv.scrollHeight;
         }, 1000);
     }, 100);
 }
@@ -191,7 +118,7 @@ function switchChat() {
             e.click();
             count--;
         }
-    })
+    });
 };
 
 function parseParams() {
@@ -621,7 +548,7 @@ function filterBoxes(boxes) {
 }
 
 function checkAll() {
-    let boxes = getChecklist.querySelectorAll("input:not(:checked)");
+    let boxes = getChecklist().querySelectorAll("input:not(:checked)");
     boxes.forEach(box => box.checked = true);
 }
 
@@ -647,6 +574,62 @@ function checkboxUpdate() {
     removeBadTranslations();
 }
  
+function createAuthorNameElement(author, authorID) {
+    let authorName = document.createElement("span");
+    authorName.textContent = author;
+    authorName.dataset.id = authorID;
+    authorName.className = "authorName";
+    return authorName;
+}
+
+function createAuthorHideButton(translation) {
+    let hide = document.createElement("span");
+    hide.style.cursor = "pointer";
+    hide.onclick = () => translation.remove();
+    hide.innerHTML = hideSVG;
+    return hide;
+}
+
+function createAuthorBanButton(authorID) {
+    let ban = document.createElement("span");
+    ban.onclick = () => {
+        allTranslators.v[authorID].checked = false;
+        checkboxUpdate();
+    };
+    ban.style.cursor = "pointer";
+    ban.innerHTML = banSVG;
+    return ban;
+}
+
+function createAuthorInfoOptions(authorID) {
+    let options = document.createElement("span");
+    options.appendChild(createAuthorHideButton(options));
+    options.appendChild(createAuthorBanButton(authorID));
+    options.style.display = "none";
+    options.className = "messageOptions";
+    return options;
+}
+
+function createAuthorInfoElement(author, authorID) {
+    let authorInfo = document.createElement("span");
+    authorInfo.appendChild(createAuthorNameElement(author, authorID));
+    authorInfo.appendChild(createAuthorInfoOptions());
+    return authorInfo;
+}
+
+function setTranslationElementCallbacks(line) {
+    line.onmouseover = () => line.querySelector(".messageOptions").style.display = "inline-block";
+    line.onmouseleave = () => line.querySelector(".messageOptions").style.display = "none";
+}
+
+function createTranslationElement(author, authorID, translation) {
+    let line = document.createElement("div");
+    line.className = "line";
+    line.textContent = translation;
+    setTranslationElementCallbacks(line);
+    line.appendChild(createAuthorInfoElement(author, authorID));
+    return line;
+}
 
 // MARK
 
@@ -894,8 +877,13 @@ function importStyle() {
     document.head.appendChild(style);
 }
 
-const closeSVG = `
-<svg class="svgButton" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg>`;
+const closeSVG = `<svg class="svgButton" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg>`;
+
+const hideSVG =  ` <svg class="hide" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 572.098 572.098" style="enable-background:new 0 0 572.098 572.098;" xml:space="preserve"> <g> <path d="M99.187,398.999l44.333-44.332c-24.89-15.037-47.503-33.984-66.763-56.379c29.187-33.941,66.053-60.018,106.947-76.426 c-6.279,14.002-9.853,29.486-9.853,45.827c0,16.597,3.696,32.3,10.165,46.476l35.802-35.797 c-5.698-5.594-9.248-13.36-9.248-21.977c0-17.02,13.801-30.82,30.82-30.82c8.611,0,16.383,3.55,21.971,9.248l32.534-32.534 l36.635-36.628l18.366-18.373c-21.206-4.186-42.896-6.469-64.848-6.469c-107.663,0-209.732,52.155-273.038,139.518L0,298.288 l13.011,17.957C36.83,349.116,66.151,376.999,99.187,398.999z"/> <path d="M459.208,188.998l-44.854,44.854c30.539,16.071,58.115,37.846,80.986,64.437 c-52.167,60.662-128.826,96.273-209.292,96.273c-10.3,0-20.533-0.6-30.661-1.744l-52.375,52.375 c26.903,6.887,54.762,10.57,83.036,10.57c107.663,0,209.738-52.154,273.038-139.523l13.011-17.957l-13.011-17.956 C532.023,242.995,497.844,212.15,459.208,188.998z"/> <path d="M286.049,379.888c61.965,0,112.198-50.234,112.198-112.199c0-5.588-0.545-11.035-1.335-16.402L269.647,378.56 C275.015,379.349,280.461,379.888,286.049,379.888z"/> <path d="M248.815,373.431L391.79,230.455l4.994-4.994l45.796-45.796l86.764-86.77c13.543-13.543,13.543-35.502,0-49.046 c-6.77-6.769-15.649-10.159-24.523-10.159s-17.754,3.384-24.522,10.159l-108.33,108.336l-22.772,22.772l-29.248,29.248 l-48.14,48.14l-34.456,34.456l-44.027,44.027l-33.115,33.115l-45.056,45.055l-70.208,70.203 c-13.543,13.543-13.543,35.502,0,49.045c6.769,6.77,15.649,10.16,24.523,10.16s17.754-3.385,24.523-10.16l88.899-88.898 l50.086-50.086L248.815,373.431z"/> </g> </svg> `;
+
+const banSVG = ` <svg class="ban" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"> <g data-name="Layer 2"> <g data-name="person-delete"> <rect width="24" height="24" opacity="0" /> <path d="M20.47 7.5l.73-.73a1 1 0 0 0-1.47-1.47L19 6l-.73-.73a1 1 0 0 0-1.47 1.5l.73.73-.73.73a1 1 0 0 0 1.47 1.47L19 9l.73.73a1 1 0 0 0 1.47-1.5z" /> <path d="M10 11a4 4 0 1 0-4-4 4 4 0 0 0 4 4z" /> <path d="M16 21a1 1 0 0 0 1-1 7 7 0 0 0-14 0 1 1 0 0 0 1 1z" /> </g> </g> </svg> `;
+
+
 
 const settingsGear = `<svg class="svgButton" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M19.43 12.98c.04-.32.07-.64.07-.98 0-.34-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.09-.16-.26-.25-.44-.25-.06 0-.12.01-.17.03l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.06-.02-.12-.03-.18-.03-.17 0-.34.09-.43.25l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98 0 .33.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.09.16.26.25.44.25.06 0 .12-.01.17-.03l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.06.02.12.03.18.03.17 0 .34-.09.43-.25l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zm-1.98-1.71c.04.31.05.52.05.73 0 .21-.02.43-.05.73l-.14 1.13.89.7 1.08.84-.7 1.21-1.27-.51-1.04-.42-.9.68c-.43.32-.84.56-1.25.73l-1.06.43-.16 1.13-.2 1.35h-1.4l-.19-1.35-.16-1.13-1.06-.43c-.43-.18-.83-.41-1.23-.71l-.91-.7-1.06.43-1.27.51-.7-1.21 1.08-.84.89-.7-.14-1.13c-.03-.31-.05-.54-.05-.74s.02-.43.05-.73l.14-1.13-.89-.7-1.08-.84.7-1.21 1.27.51 1.04.42.9-.68c.43-.32.84-.56 1.25-.73l1.06-.43.16-1.13.2-1.35h1.39l.19 1.35.16 1.13 1.06.43c.43.18.83.41 1.23.71l.91.7 1.06-.43 1.27-.51.7 1.21-1.07.85-.89.7.14 1.13zM12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 6c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"/></svg>`;
 
