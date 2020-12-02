be1678fa36830e3135475e7e71e378806ea3c393
r2dev2bb8 <ronak.badhe@gmail.com>
2020-12-01 20:50:41 -0800

    added parseTranslation tests

--- tests/filter.js
+++ tests/filter.js
@@ -1,8 +1,15 @@
 const assert = require('assert').strict;
 const {parseTranslation, isLangMatch} = require("../LiveTL/js/filter.js")
 
+let tests = {
+    value: 0,
+};
+
 function compareTranslationMatches(other, correct) {
-    return other.lang == correct.lang && other.msg == correct.msg;
+    if (other && correct) {
+        return other.lang == correct.lang && other.msg == correct.msg;
+    }
+    return other == correct;
 }
 
 function testParseTranslation() {
@@ -15,11 +22,48 @@ function testParseTranslation() {
             msg: "(es) - Hola eso",
             correct: { lang: "es", msg: "Hola eso" },
         },
+        {
+            msg: "jp - リーーーーーーー",
+            correct: { lang: "jp", msg: "リーーーーーーー" },
+        },
+        {
+            msg: "en:Test translation",
+            correct: { lang: "en", msg: "Test translation" },
+        },
+        {
+            msg: "No translation",
+            correct: undefined,
+        }
     ];
 
-    testCases.forEach((tc) => {
-        assert.ok(true);
+    testCases.forEach(({ msg, correct }) => {
+        assert.ok(
+            compareTranslationMatches(parseTranslation(msg), correct),
+            `Couldn't parse translation of ${msg} to ${correct}`,
+        );
     });
+    process.stdout.write(".");
+    tests.value++;
+}
+
+function testLangMatch() {
+    const testCases = [
+        {
+            testCase: ["En", { code: "en", name: "English", lang: "English" }],
+            match: true,
+        },
+    ];
+    testCases.forEach(({ testCase, match }) => {
+        assert.equal(
+            isLangMatch(...testCase), match,
+            `Incorrect language match between ${testCase[0]} and ${testCase[1]}`,
+        );
+    });
+    process.stdout.write(".");
+    tests.value++;
 }
 
 testParseTranslation();
+testLangMatch();
+
+console.log(`Finished ${tests.value} unit tests for filter.js`);
