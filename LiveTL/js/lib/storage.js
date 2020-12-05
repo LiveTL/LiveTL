46f1cce85ba7dbc0da69a90b79268e0fdae0924e
r2dev2bb8 <ronak.badhe@gmail.com>
2020-12-04 16:03:42 -0800

    can store and get user status

--- LiveTL/js/lib/storage.js
+++ LiveTL/js/lib/storage.js
@@ -1,10 +1,24 @@
 async function getStorage(key) {
-  return await browser.storage.local.get(key).then(p => p[key]);
+  const result = await browser.storage.local.get(key);
+  return result ? result[key] : result;
 }
 
 async function setStorage(key, value) {
   let obj = {}
   obj[key] = value;
   return await browser.storage.local.set(obj);
+}
+
+async function isChecked(userid) {
+  const status = await getStorage(userid);
+  if (status) {
+    return status.checked;
+  }
+  const allCheckmark = await getStorage('allTranslatorID');
+  return allCheckmark ? allCheckmark.checked : true;
+}
+
+async function saveUserStatus(userid, checked) {
+  return await setStorage(userid, { checked });
 }
 
