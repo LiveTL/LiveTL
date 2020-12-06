e36db106e09b1dcb222824fe7641a456986a86cf
KentoNishi <kento24gs@outlook.com>
2020-12-06 11:22:00 -0800

    Added loading indicator text

--- changelogs/index.js
+++ changelogs/index.js
@@ -8,9 +8,9 @@ function parseParams(loc) {
 
 (async () => {
   let params = parseParams();
+  document.querySelector('.version').textContent = params.version;
   let details = await fetch('https://api.github.com/repos/KentoNishi/LiveTL/releases/tags/' + params.version);
   details = await details.json();
-  document.querySelector('.version').textContent = params.version;
   document.querySelector('.changelogs').textContent = details.body;
   document.querySelector('.published').textContent = (new Date(details.published_at)).toString();
 })();
