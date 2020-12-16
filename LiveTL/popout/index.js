5a2705215ca84835cb3e2a37da9ac815157f8bc8
Grumpy Bear <grumpybear4257@gmail.com>
2020-12-16 01:44:03 -0700

    fix timestamps

--- LiveTL/popout/index.js
+++ LiveTL/popout/index.js
@@ -2,10 +2,10 @@ window.addEventListener('message', m => {
   if (typeof m.data == 'object') {
     switch (m.data.type) {
       case 'messageChunk':
-        console.log(m.data.messages);
         m.data.messages.forEach(onNewMessage);
         break;
     }
   }
 });
+
 runLiveTL();
