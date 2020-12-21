ad9a283154204adecf597f2dad44bc3439b593c4
r2dev2bb8 <ronak.badhe@gmail.com>
2020-12-20 23:33:20 -0800

    test if buttons are added in vods

--- tests/selenium/routines.py
+++ tests/selenium/routines.py
@@ -1,6 +1,7 @@
 from wrappers import retry_every_interval, web_test
 
 chilled_cow = "https://www.youtube.com/watch?v=5qap5aO4i9A"
+peko_kiara = "https://www.youtube.com/watch?v=c747jYku6Eo&t=926"
 
 
 class TestCases:
@@ -21,8 +22,15 @@ class TestCases:
         assert len(ltlbuttons) == 2, "LiveTL buttons not inserted in live"
 
     @web_test
+    @retry_every_interval()
+    def test_vod_button_insertion(web):
+        go_to_website(web, peko_kiara)
+        ltlbuttons = web.find_elements_by_css_selector(".liveTLBotan")
+        assert len(ltlbuttons) == 2, "LiveTL buttons not inserted in vod"
+
+    @web_test
     def test_livetl_panel_layout(web):
-        go_to_website(web, chilled_cow)
+        go_to_video_with_buttons(web, chilled_cow)
         buttons = web.find_elements_by_css_selector(".liveTLBotan")
         buttons[0].click()
         iframes = web.find_elements_by_css_selector("iframe")
@@ -44,3 +52,10 @@ class TestCases:
 def go_to_website(web, url):
     if web.current_url != url:
         web.get(url)
+
+
+@retry_every_interval()
+def go_to_video_with_buttons(web, url):
+    go_to_website(web, chilled_cow)
+    ltlbuttons = web.find_elements_by_css_selector(".liveTLBotan")
+    assert len(ltlbuttons) == 2, "LiveTL buttons not inserted in live"
