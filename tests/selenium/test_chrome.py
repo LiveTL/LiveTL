e75179891a79ea929deea0e05778f7e345d66cd0
r2dev2bb8 <ronak.badhe@gmail.com>
2020-12-21 00:30:51 -0800

    added support for DISABLE_CHROME environment variable

--- tests/selenium/test_chrome.py
+++ tests/selenium/test_chrome.py
@@ -1,6 +1,9 @@
+import os
+
 import chrome
 import routines
 
 
-class TestChrome(routines.TestCases):
-    browser = chrome
+if not os.environ.get("DISABLE_CHROME"):
+    class TestChrome(routines.TestCases):
+        browser = chrome
