ec9c21f2c1595df607968865e06a255ae0a87bd1
KentoNishi <kento24gs@outlook.com>
2020-12-26 12:54:35 -0800

    Filter by intent

--- LiveTL-Android/app/src/main/java/com/android/livetl/MainActivity.java
+++ LiveTL-Android/app/src/main/java/com/android/livetl/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.view.Window;
@@ -19,7 +21,12 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-    int flags = View.SYSTEM_UI_FLAG_FULLSCREEN;
+    int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        | View.SYSTEM_UI_FLAG_FULLSCREEN
+        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
@@ -28,14 +35,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_LANDSCAPE);
         getWindow().getDecorView().setSystemUiVisibility(flags);
-        loadWebview();
+        onNewIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent){
+        String action = intent.getAction();
+        String type = intent.getType();
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
+                String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+                String[] split = sharedText.split("/");
+                sharedText = split[split.length - 1];
+                loadWebview("file:///android_asset/index.html?v=" + sharedText);
+            }
+        } else {
+            loadWebview("https://kentonishi.github.io/LiveTL/about");
+        }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    void loadWebview(){
+    void loadWebview(String url){
         WebView wv = (WebView) findViewById(R.id.mainWebview);
         wv.setWebViewClient(new WebViewClient());
-        wv.loadUrl("file:///android_asset/index.html");
+        wv.loadUrl(url);
         WebSettings s = wv.getSettings();
         s.setJavaScriptEnabled(true);
         s.setDomStorageEnabled(true);
@@ -50,18 +73,19 @@ public class MainActivity extends AppCompatActivity {
             "(KHTML, like Gecko) Chrome/89.0.4346.0 Safari/537.36 Edg/89.0.731.0"
         );
         wv.setScrollBarStyle(WebView.SCROLLBARS_INSIDE_INSET);
+        wv.setOverScrollMode(View.OVER_SCROLL_NEVER);
         wv.setScrollbarFadingEnabled(false);
         wv.setWebContentsDebuggingEnabled(true);
         wv.setInitialScale(getResources().getDisplayMetrics().densityDpi);
-//        View root = wv.getRootView();
-//        ViewTreeObserver treeObserver = root.getViewTreeObserver();
-//        treeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-//            @Override
-//            public void onGlobalLayout() {
-//                root.getRootView().getViewTreeObserver().removeOnGlobalLayoutListener(this);
-//                Utils.updateGestureExclusion(MainActivity.this);
-//            }
-//        });
+        View root = wv.getRootView();
+        ViewTreeObserver treeObserver = root.getViewTreeObserver();
+        treeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                root.getRootView().getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                Utils.updateGestureExclusion(MainActivity.this);
+            }
+        });
     }
 
     @SuppressLint("NewApi")
--- LiveTL-Android/app/src/main/java/com/android/livetl/Utils.java
+++ LiveTL-Android/app/src/main/java/com/android/livetl/Utils.java
@@ -15,8 +15,8 @@ public class Utils {
     public static void updateGestureExclusion(AppCompatActivity activity) {
         if (Build.VERSION.SDK_INT < 29) return;
         exclusionRects.clear();
-        int width = getScreenWidth(activity) + 250;
-        int height = getScreenHeight(activity) + 250;
+        int width = getScreenWidth(activity);
+        int height = getScreenHeight(activity);
         exclusionRects.add(new Rect(0, 0, width, height));
 
         activity.findViewById(android.R.id.content).setSystemGestureExclusionRects(exclusionRects);
