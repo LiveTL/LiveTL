90f76bcd796b8cb0c1d0c4d7b00c07dbe9d6697a
KentoNishi <kento24gs@outlook.com>
2020-12-27 09:17:07 -0800

    Logo and colors

--- LiveTL-Android/app/src/main/java/com/android/livetl/MainActivity.java
+++ LiveTL-Android/app/src/main/java/com/android/livetl/MainActivity.java
@@ -7,6 +7,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
