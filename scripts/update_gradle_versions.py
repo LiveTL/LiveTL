e55c7b421e50fde0070ead73dfd32e3e9728abfc
r2dev2 <ronak.badhe@gmail.com>
2020-12-31 21:44:04 -0800

    changed to use ghub api to check no. releases

--- scripts/update_gradle_versions.py
+++ scripts/update_gradle_versions.py
@@ -1,3 +1,4 @@
+import requests
 from pathlib import Path
 import os
 import re
@@ -6,11 +7,13 @@ new_gradle = []
 
 build_gradle = Path('.') / "LiveTL-Android/app/build.gradle"
 
+releases = len(requests.get("https://api.github.com/repos/KentoNishi/LiveTL/releases").json())
+
 with open(build_gradle, 'r') as fin:
     for line in fin:
         if "versionCode" in line:
-            version_code = int(re.search(r"(\d+)", line).group(1))
-            new_line = re.sub(r"\d+", f"{version_code + 1}", line)
+            version_code = releases - 28
+            new_line = re.sub(r"\d+", f"{version_code}", line)
         elif "versionName" in line:
             version_name = re.sub(r"\"", "", os.environ["VERSION"])
             new_line = re.sub(r"[\d\.]+", version_name, line)
