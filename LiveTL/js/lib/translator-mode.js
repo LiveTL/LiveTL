a55e37fd6b9cfdce49d4439e03da6104c61790a5
r2dev2 <ronak.badhe@gmail.com>
2021-01-01 18:51:02 -0800

    stopped polluting namespace

--- LiveTL/js/lib/translator-mode.js
+++ LiveTL/js/lib/translator-mode.js
@@ -1,3 +1,25 @@
-async function runTranslatorModeYTC() {
-  ;
+// TranslatorMode.run()
+// Make sure to setTimeout when running at initialization
+
+const TranslatorMode = (() => {
+
+async function run() {
+  addTextToYTC('[en] ');
 }
+
+function addTextToYTC(text) {
+  let [container, chatbox] = document.querySelectorAll('#input');
+  container.setAttribute('has-text', '');
+  chatbox.textContent = text;
+}
+
+function observeYTC(observer) {
+  observer.observe(document.querySelector('#input'), { attributes: true });
+}
+
+function observerCallback(attributeChanged) {
+  console.log(attributeChanged);
+}
+
+return { run };
+})();
