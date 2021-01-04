5a80cc36990f5a68c80aba443712b2bcaa4f6d6e
r2dev2 <ronak.badhe@gmail.com>
2021-01-04 14:32:45 -0800

    only add favicon when marine detected

--- LiveTL/js/lib/marine.js
+++ LiveTL/js/lib/marine.js
@@ -1,20 +1,47 @@
 const Marine = (() => {
-  const faviconElement = document.querySelector("link[rel*='icon']");
-  const oldFaviconURL = faviconElement.href;
+  let oldFaviconURL = null;
+
+  function isHolni() {
+    return isHolniParam() || isHolniYT();
+  }
+
+  function isHolniParam() {
+    const params = parseParams(window.location.href);
+    return params.marine == 'holni';
+  }
+
+  function isHolniYT() {
+    return document
+      .querySelectorAll('a[href="/channel/UCCzUftO8KOVkV4wQG1vkUvg"]')
+      .length > 0;
+  }
+
+  function holniParam() {
+    return isHolni() ? 'marine=holni' : 'marine=not_holni';
+  }
 
   async function makeFaviconBL() {
-    faviconElement.href = await getMarineFaviconURL();
+    oldFaviconURL = oldFaviconURL || faviconElement().href;
+    faviconElement().href = await getMarineFaviconURL();
   }
 
   function resetFavicon() {
-    faviconElement.href = oldFaviconURL;
+    faviconElement().href = oldFaviconURL;
+  }
+
+  function faviconElement() {
+    return document.querySelector("link[rel*='icon']");
   }
 
   async function getMarineFaviconURL() {
     return await getWAR('icons/blfavicon.ico');
   }
 
-  return { makeFaviconBL, resetFavicon };
+  return { holniParam, isHolni, makeFaviconBL, resetFavicon };
 })();
 
-Marine.makeFaviconBL().then(Marine.resetFavicon);
+if (Marine.isHolni()) {
+  Marine.makeFaviconBL();
+} else {
+  Marine.resetFavicon();
+}
