4073135567943cc0282c1943be29482afbda1391
KentoNishi <kento24gs@outlook.com>
2021-01-04 10:06:13 -0800

    Revert "Button color for holotools"
    This reverts commit bc96fffe0d940bfe6dc396f1f35220fe8dc6f027.

--- LiveTL/js/frame.js
+++ LiveTL/js/frame.js
@@ -348,7 +348,7 @@ async function insertLiveTLButtons(isHolotools = false) {
       } else {
         window.location.href = tlurl;
       }
-    }, isHolotools ? undefined : `var(--paper-indigo-300)`);
+    }, `var(--paper-indigo-300)`);
 }
 
 function isReplayChat() {
