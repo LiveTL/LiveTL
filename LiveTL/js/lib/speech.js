3b7f8cb6dbcb1ed3a592bf258c63a01f4c43db06
Aaron Thapa <31830171+yoyoyonono@users.noreply.github.com>
2021-01-07 01:42:38 -0500

    semistandard だね。。。 (#133)

--- LiveTL/js/lib/speech.js
+++ LiveTL/js/lib/speech.js
@@ -1,26 +1,25 @@
 // There is no current way to fire a callback on the end of tts
 // The async declaration is there for future possible support
-async function speak(text) {
+async function speak (text) {
   if (speechUnlocked) {
     const utterance = new SpeechSynthesisUtterance(text);
     speechSynthesis.speak(utterance);
   }
 }
 
-async function shouldSpeak() {
+async function shouldSpeak () {
   return await getStorage('speechSynth');
 }
 
-async function checkAndSpeak(text) {
+async function checkAndSpeak (text) {
   if (await shouldSpeak()) {
     await speak(text);
   }
 }
 
-function unlockSpeech() {
+function unlockSpeech () {
   speechUnlocked = true;
 }
 
 const speechSynthDefault = false;
 let speechUnlocked = false;
-
