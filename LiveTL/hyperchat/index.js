8ad9b2a2ad903aac0bf1c5e111e0f36105061e24
KentoNishi <kento24gs@outlook.com>
2021-01-26 19:17:32 -0800

    Attempt dynamic mount

--- LiveTL/hyperchat/index.js
+++ LiveTL/hyperchat/index.js
@@ -1,2 +1,7 @@
-// no clue how to load hyperchat
-// @grumpy help
+(async () => {
+  const temp = await (await fetch(await getWAR('/hyperchat/index.html#/chat'))).text();
+  new Vue({
+    data: () => ({}),
+    template: temp
+  }).$mount('#app');
+})();
