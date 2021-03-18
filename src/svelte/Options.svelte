a086dee725f0772ab5648f41ea5c8879f302ecab
KentoNishi <kento24gs@outlook.com>
2021-03-17 20:00:25 -0700

    No overflowing body

--- src/svelte/Options.svelte
+++ src/svelte/Options.svelte
@@ -1,17 +1,20 @@
 <script>
-  import { MaterialApp } from 'svelte-materialify';
+  import { MaterialApp } from "svelte-materialify";
 
   import Settings from "./Settings.svelte";
 </script>
 
 <div class="options">
-<MaterialApp>
-  <Settings />
-</MaterialApp>
+  <MaterialApp>
+    <Settings />
+  </MaterialApp>
 </div>
 
 <style>
   .options {
     margin: 20px;
+  }
+  :global(html body) {
+    height: auto !important;
   }
 </style>
