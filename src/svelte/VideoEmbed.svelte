6095c64f3971e74b3e736bc4da45820bca3e0696
KentoNishi <kento24gs@outlook.com>
2021-03-18 20:17:33 -0700

    autoplay

--- src/svelte/VideoEmbed.svelte
+++ src/svelte/VideoEmbed.svelte
@@ -1,11 +1,15 @@
 <script>
   import YouTubeIframeLoader from "youtube-iframe";
   export let videoId;
-  YouTubeIframeLoader.load(function (YT) {
+  YouTubeIframeLoader.load((YT) => {
     new YT.Player("player", {
       height: "100%",
       width: "100%",
       videoId,
+      autoplay: 1,
+      playerVars: {
+        autoplay: 1,
+      },
     });
   });
 </script>
