a3ca6af3a53cf656af3212478e4c594f518997e2
KentoNishi <kento24gs@outlook.com>
2021-03-18 23:52:06 -0700

    Test runs!

---
+++ tests/test.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+// NOTE: jest-dom adds handy assertions to Jest and it is recommended, but not required.
+import '@testing-library/jest-dom/extend-expect';
+
+import { render /*, fireEvent*/ } from '@testing-library/svelte';
+
+import Wrapper from '../src/svelte/Wrapper.svelte';
+
+test('shows proper heading when rendered', () => {
+  /* const { getByText } =*/ render(Wrapper, );
+//   expect(getByText('Hello World!')).toBeInTheDocument();
+});
+
+// // Note: This is as an async test as we are using `fireEvent`
+// test('changes button text on click', async () => {
+//   const { getByText } = render(Comp, { name: 'World' });
+//   const button = getByText('Button');
+
+//   // Using await when firing events is unique to the svelte testing library because
+//   // we have to wait for the next `tick` so that Svelte flushes all pending state changes.
+//   await fireEvent.click(button);
+
+//   expect(button).toHaveTextContent('Button Clicked');
+// });
