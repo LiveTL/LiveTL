ce5a0beaa7753388450e3d03ed2b00a8e103f60d
r2dev2 <ronak.badhe@gmail.com>
2021-03-19 19:46:09 -0700

    smol scrollbar

--- src/components/Wrapper.svelte
+++ src/components/Wrapper.svelte
@@ -22,3 +22,29 @@ position: absolute;
 >
   <slot />
 </div>
+
+<style>
+/* width */
+
+::-webkit-scrollbar {
+    width: 4px;
+}
+
+/* Track */
+
+::-webkit-scrollbar-track {
+    background: transparent;
+}
+
+/* Handle */
+
+::-webkit-scrollbar-thumb {
+    background: #888;
+}
+
+/* Handle on hover */
+
+::-webkit-scrollbar-thumb:hover {
+    background: #555;
+}
+</style>
