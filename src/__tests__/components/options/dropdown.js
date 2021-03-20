abcbff513977bc1dc3505aad0b62924deafb0bd6
r2dev2 <ronak.badhe@gmail.com>
2021-03-20 01:41:47 -0700

    finish dropdown tests

--- src/__tests__/components/options/dropdown.js
+++ src/__tests__/components/options/dropdown.js
@@ -13,13 +13,48 @@ describe('Dropdown', () => {
     { name: 'test2', value: 'test2Value' }
   ];
 
+  it('shows label', () => {
+    const { getByText } = render(Dropdown, {
+      name: 'Dropdown',
+      items,
+      store: writable(items[0].name)
+    });
+    const label = getByText('Dropdown');
+    expect(label).toBeInTheDocument();
+  });
+
+  it('shows choices', async () => {
+    const store = writable(items[0].value);
+    const { getByText, getByDisplayValue } = render(Dropdown, { items, store });
+    const button = getByDisplayValue(items[0].value);
+    await fireEvent.click(button);
+    items.forEach(item => expect(getByText(item.name)).toBeInTheDocument());
+  });
+
+  it('updates with the store', async () => {
+    const store = writable(items[0].value);
+    const { getByDisplayValue } = render(Dropdown, { items, store });
+    await store.set(items[1].value);
+    setTimeout(() => {
+      expect(getByDisplayValue(items[1].value)).toBeInTheDocument();
+    }, 1000);
+  });
+
+  it('updates the store', async () => {
+    const store = writable(items[0].value);
+    const { getByText, getByDisplayValue } = render(Dropdown, { items, store });
+    await fireEvent.click(getByDisplayValue(items[0].value));
+    await fireEvent.click(getByText(items[1].name));
+    expect(get(store)).toEqual(items[1].value);
+  });
+
   it('is blue', async () => {
     const { getByText, getByDisplayValue } = render(Dropdown, {
       name: 'Dropdown',
       items,
-      store: writable('test')
+      store: writable(items[0].value)
     });
-    const button = getByDisplayValue('test');
+    const button = getByDisplayValue(items[0].value);
     const label = getByText('Dropdown');
     await fireEvent.click(button);
     setTimeout(() => {
