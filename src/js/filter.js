6cd43130cc1e33c04d175e5439703c7a280c4e69
r2dev2 <ronak.badhe@gmail.com>
2021-03-21 01:30:40 -0700

    got the filter and ported its unit tests

---
+++ src/js/filter.js
@@ -0,0 +1,82 @@
+const MAX_LANG_TAG_LEN = 7;
+
+const langTokens = [['[', ']'], ['{', '}'], ['(', ')'], ['|', '|'], ['<', '>'], ['【', '】'], ['「', '」'], ['『', '』'], ['〚', '〛'], ['（', '）'], ['〈', '〉'], ['⁽', '₎']];
+const startLangTokens = langTokens.flatMap(e => e[0]);
+const tokenMap = Object.fromEntries(langTokens);
+
+const transDelimiters = ['-', ':'];
+const langSplitRe = /[^A-Za-z]/;
+
+// const langSplitRe = /[^A-Za-z\/\ \-\:\.\|\／]/;
+/**
+ * Parses translation
+ *
+ * @param message the message to parse
+ * @return undefined or
+ * {
+ *    lang: lang code
+ *    msg: message
+ * }
+ */
+export function parseTranslation(message) {
+  const trimmed = removeEmojis(message.trim());
+
+  // try bracket trans blocks first - '[lang]', '[lang] -'
+  const leftToken = trimmed[0];
+  const rightToken = tokenMap[leftToken];
+
+  const righTokenIndex = trimmed.indexOf(rightToken);
+
+  if (righTokenIndex !== -1) {
+    const startsWithLeftToken = startLangTokens.includes(trimmed[0]);
+
+    if (startsWithLeftToken) {
+      const lang = trimmed.slice(1, righTokenIndex);
+      let msg = trimmed.slice(righTokenIndex + 1).trim();
+
+      // remove potential trailing dash
+      if (msg[0] === '-' || msg[0] === ':') {
+        msg = msg.slice(1).trim();
+      }
+
+      return {
+        lang,
+        msg
+      };
+    }
+  }
+
+  // try all delims
+  for (const delim of transDelimiters) {
+    const idx = trimmed.indexOf(delim);
+
+    if (idx !== -1 && idx < MAX_LANG_TAG_LEN) {
+      const lang = trimmed.slice(0, idx).trim().replace(/\W/g, '');
+      const msg = trimmed.slice(idx + 1).trim();
+
+      return {
+        lang,
+        msg
+      };
+    }
+  }
+
+  return undefined;
+};
+
+function removeEmojis(str) {
+  return str;
+  // no need to remove emoji, issue was invalid chars
+  // return str.replace(/([#0-9]\u20E3)|[\xA9\xAE\u203C\u2047-\u2049\u2122\u2139\u3030\u303D\u3297\u3299][\uFE00-\uFEFF]?|[\u2190-\u21FF][\uFE00-\uFEFF]?|[\u2300-\u23FF][\uFE00-\uFEFF]?|[\u2460-\u24FF][\uFE00-\uFEFF]?|[\u25A0-\u25FF][\uFE00-\uFEFF]?|[\u2600-\u27BF][\uFE00-\uFEFF]?|[\u2900-\u297F][\uFE00-\uFEFF]?|[\u2B00-\u2BF0][\uFE00-\uFEFF]?|(?:\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDEFF])[\uFE00-\uFEFF]?/g, '');
+}
+
+export function isLangMatch(textLang, currentLang) {
+  textLang = textLang.toLowerCase().split(langSplitRe).filter(s => s !== '');
+  return textLang.some(s => (
+    s && s.length >= 2 && (
+      currentLang.name.toLowerCase().startsWith(s) ||
+      s === currentLang.code ||
+      currentLang.lang.toLowerCase().startsWith(s)
+    )
+  ));
+}
