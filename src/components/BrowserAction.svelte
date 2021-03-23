f206e71f20f2a7b88154baa45f3d9f1429f25673
KentoNishi <kento24gs@outlook.com>
2021-03-23 01:06:52 -0700

    Working browser action

---
+++ src/components/BrowserAction.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Options from "../components/Options.svelte";
+  import Wrapper from "./Wrapper.svelte";
+  import { MaterialApp } from "svelte-materialify/src";
+  import { livetlZoom } from "../js/store.js";
+</script>
+
+<MaterialApp theme="dark">
+  <Wrapper zoom={$livetlZoom}>
+    <Options isStandalone />
+  </Wrapper>
+</MaterialApp>
+
+<style>
+  :global(html) {
+    min-width: 500px !important;
+    min-height: 400px !important;
+  }
+</style>
--- src/js/pages/options.js
+++ src/js/pages/options.js
@@ -1,4 +1,4 @@
-import App from '../../components/Popout.svelte';
+import App from '../../components/BrowserAction.svelte';
 
 const app = new App({
   target: document.body,
