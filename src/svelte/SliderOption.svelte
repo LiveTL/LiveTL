c1b852f15c2a0204bb8ea3e0341a0c3bad3da1b6
r2dev2 <ronak.badhe@gmail.com>
2021-04-10 20:51:43 -0700

    persistent slider option

--- src/svelte/SliderOption.svelte
+++ src/svelte/SliderOption.svelte
@@ -1,16 +1,21 @@
 <script>
-  import { Slider } from "svelte-materialify/src";
+  import { Button, Icon, Slider } from "svelte-materialify/src";
+  import { mdiHome } from "@mdi/js";
   import { Browser, BROWSER } from "../js/web-constants.js";
 
   export let min = BROWSER == Browser.ANDROID ? 0.25 : 0.5;
   export let max = BROWSER == Browser.ANDROID ? 1.5 : 2;
   export let name = "";
   export let store = null;
+  export let color = "blue";
 
-  let value = store.get();
+  let diff = max - min;
+  $: value = Math.round(($store - min) * 100 / diff + min);
+  $: scaledBack = (value - min) * diff / 100 + min;
+  $: store.set(scaledBack);
 </script>
 
-<Slider bind:value {min} {max} color="blue">
+<Slider thumb bind:value {color} thumbClass={color}>
   {name}:
 </Slider>
 
