0f101e26e3795d061eb524ee1691d3e246b1564b
KentoNishi <kento24gs@outlook.com>
2021-04-11 16:18:34 -0700

    Fixed frozen update panel

--- src/components/Updates.svelte
+++ src/components/Updates.svelte
@@ -1,8 +1,8 @@
 <script>
-  import { Button } from 'svelte-materialify';
-  import { onMount, onDestroy } from 'svelte';
-  import { lastVersion } from '../js/store.js';
-  import Dialog from './Dialog.svelte';
+  import { Button } from "svelte-materialify";
+  import { onMount, onDestroy } from "svelte";
+  import { lastVersion } from "../js/store.js";
+  import Dialog from "./Dialog.svelte";
 
   const manifest = chrome.runtime.getManifest();
   const version = manifest.version;
@@ -22,41 +22,39 @@
   let dialog;
   $: if (dialog) {
     dialog
-      .querySelector('.s-overlay')
-      .addEventListener('click', () => unsetLastVersion());
+      .querySelector(".s-overlay")
+      .addEventListener("click", () => unsetLastVersion());
   }
 
   $: active = $lvLoaded && $lastVersion != version;
   onDestroy(() => lastVersion.set(active ? $lastVersion : version));
 </script>
 
-{#if active}
-  <div bind:this={dialog}>
-    <Dialog bind:active>
-      <span class="centered">
-        <h1>New Update!</h1>
-        <h2>Here's what's new in LiveTL version {version}:</h2>
-      </span>
-      <span class="left">
-        <svelte:component this={Changelogs} />
-      </span>
-      <span class="centered">
-        <h2 style="margin: 25px;">
-          If you like this update, please consider sharing this information with
-          your friends! We'd really appreciate it :)
-        </h2>
-      </span>
-      <Button
-        transition
-        size="default"
-        class="blue"
-        on:click={() => unsetLastVersion()}
-      >
-        Let's Go!
-      </Button>
-    </Dialog>
-  </div>
-{/if}
+<div bind:this={dialog}>
+  <Dialog bind:active>
+    <span class="centered">
+      <h1>New Update!</h1>
+      <h2>Here's what's new in LiveTL version {version}:</h2>
+    </span>
+    <span class="left">
+      <svelte:component this={Changelogs} />
+    </span>
+    <span class="centered">
+      <h2 style="margin: 25px;">
+        If you like this update, please consider sharing this information with
+        your friends! We'd really appreciate it :)
+      </h2>
+    </span>
+    <Button
+      transition
+      size="default"
+      class="blue"
+      on:click={() => unsetLastVersion()}
+    >
+      Let's Go!
+    </Button>
+  </Dialog>
+</div>
 
 <style>
   h1 {
