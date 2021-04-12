155896569cf656054520f56aaa7c3cb4895ea5a6
Candygoblen123 <andrew.glaze76@gmail.com>
2021-04-12 14:31:49 -0400

    Change toggles to checkboxes

--- src/components/options/Toggle.svelte
+++ src/components/options/Toggle.svelte
@@ -1,9 +1,9 @@
 <script>
-  import { Switch } from 'svelte-materialify/src';
+  import { Checkbox } from 'svelte-materialify/src';
   export let name = '';
   export let store = null;
 </script>
 
-<Switch on:change={store.update((n) => !n)} color="blue" checked={$store} dense>
+<Checkbox on:change={store.update((n) => !n)} color="blue" checked={$store} dense>
   <span class="option-label">{name}</span>
-</Switch>
+</Checkbox>
