2837292c24abc2f3af4ef5b385c934372bf13664
r2dev2 <ronak.badhe@gmail.com>
2021-04-22 15:23:20 -0700

    Fix #177

--- utils/package.js
+++ utils/package.js
@@ -3,10 +3,20 @@ const { spawn } = require('child_process');
 // eslint-disable-next-line no-unused-vars
 const shouldBeInRoot = process.argv.indexOf('firefox') != -1;
 
+const cmds = [
+  'mkdir -p dist',
+  'cd build',
+  'zip -9r ../dist/LiveTL-Firefox.zip .',
+  'cp ../dist/LiveTL-Firefox.zip ../dist/LiveTL-Chrome.zip',
+  'zip -d ../dist/LiveTL-Chrome.zip manifest.json',
+  'printf "@ manifest.chrome.json\\n@=manifest.json\\n" | zipnote -w ../dist/LiveTL-Chrome.zip',
+  'zip -d ../dist/LiveTL-Firefox.zip manifest.chrome.json'
+];
+
 spawn(
   'sh',
   [
     '-c',
-    'mkdir -p dist && cd build && zip -9r ../dist/LiveTL.zip .'
+    cmds.join(' && ')
   ]
 );
