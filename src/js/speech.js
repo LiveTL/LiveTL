3158aa1a4d8da3ccda11bfa8fa9dc2e6e314426b
Kento Nishi <kento24gs@outlook.com>
2021-05-03 08:32:53 -0700

    Don't cancel speech synth

--- src/js/speech.js
+++ src/js/speech.js
@@ -3,7 +3,7 @@ import { doSpeechSynth, speechVolume } from './store.js';
 let utterance = null;
 
 export function speak(text, volume=0) {
-  speechSynthesis.cancel();
+  // speechSynthesis.cancel();
   utterance = new SpeechSynthesisUtterance(text);
   utterance.volume = volume || speechVolume.get();
   speechSynthesis.speak(utterance);
