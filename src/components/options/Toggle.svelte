62d62d80c6903b9263a8acb61a3d7cb10fea8567
r2dev2 <ronak.badhe@gmail.com>
2021-05-03 14:35:00 -0700

    Fix #200

--- src/components/options/Toggle.svelte
+++ src/components/options/Toggle.svelte
@@ -2,8 +2,11 @@
   import { Checkbox } from 'svelte-materialify/src';
   export let name = '';
   export let store = null;
+
+  $: value = $store;
+  $: store.set(value);
 </script>
 
-<Checkbox bind:checked={$store} color="blue">
+<Checkbox bind:checked={value} color="blue">
   <span class="option-label">{name}</span>
 </Checkbox>
