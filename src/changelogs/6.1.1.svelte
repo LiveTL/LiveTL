96a7110a3951ce3a72af6faa0328ac7738faa8ae
Kento Nishi <kento24gs@outlook.com>
2021-05-11 17:08:37 -0700

    Better popup for 6.1.1

--- src/changelogs/6.1.1.svelte
+++ src/changelogs/6.1.1.svelte
@@ -8,15 +8,27 @@
   <V6 />
 
   <ExpansionPanel>
-    <span slot="header">Bug fixes</span>
+    <span slot="header">Bug fixes and known issues</span>
     <div>
-      This build includes several bug fixes.
+      This build fixes the following:
       <ul>
         <li>
           The fullscreen button now correctly full-screens the entire LiveTL
           interface.
         </li>
       </ul>
+      Known issues with this build:
+      <ul>
+        <li>
+          When the chat side is set to LEFT, the panel size can be enlarged but
+          not reduced.
+        </li>
+        <li>Vertical mode has not yet been implemented.</li>
+        <li>Vertical mode has not yet been implemented.</li>
+      </ul>
+      Please use our
+      <a href="https://discord.gg/uJrV3tmthg" target="_blank">Discord server</a
+      > for reporting any bugs, issues, or suggestions!
     </div>
   </ExpansionPanel>
 </ExpansionPanels>
