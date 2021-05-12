78b310bf7c6f49551a5f046f9510ac825c3be2b5
Kento Nishi <kento24gs@outlook.com>
2021-05-11 17:00:55 -0700

    Changelogs for fullscreen fix

---
+++ src/changelogs/6.1.1.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { ExpansionPanel, ExpansionPanels } from 'svelte-materialify';
+  import V6 from './common/6.1.svelte';
+  let value = [1];
+</script>
+
+<ExpansionPanels bind:value>
+  <V6 />
+
+  <ExpansionPanel>
+    <span slot="header">Bug fixes</span>
+    <div>
+      This build includes several bug fixes.
+      <ul>
+        <li>
+          The fullscreen button now correctly full-screens the entire LiveTL
+          interface.
+        </li>
+      </ul>
+    </div>
+  </ExpansionPanel>
+</ExpansionPanels>
--- src/changelogs/versionMap.js
+++ src/changelogs/versionMap.js
@@ -1,10 +1,17 @@
 export default function(version) {
   const v = version.split('.').map(d => parseInt(d));
-  if (v[0] == 6) {
+  switch (v[0]) {
+  case 6: {
     switch (v[1]) {
     case 0: return '6.0';
-    case 1: return '6.1';
+    case 1: {
+      switch(v[2]){
+      case 0: return '6.1.0';
+      case 1: return '6.1.1';
+      }
     }
+    }
+  }
   }
   return '';
 }
