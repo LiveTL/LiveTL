842d284d1d2c2d1153b7fa23efc888def400515c
Kento Nishi <kento24gs@outlook.com>
2021-05-20 20:44:26 -0700

    Init 6.1.9. Not releasing now though

---
+++ src/changelogs/6.1.9.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { ExpansionPanel, ExpansionPanels } from 'svelte-materialify';
+    let value = [0];
+</script>
+
+<ExpansionPanels bind:value>
+  <ExpansionPanel>
+    <span slot="header">Welcome page on install</span>
+    <div>
+      A welcome page is shown upon first install. This change does not affect
+      existing installations.
+    </div>
+  </ExpansionPanel>
+</ExpansionPanels>
