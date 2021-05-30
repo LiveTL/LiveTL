1d1d95f144f6111b83a648f8e2e0ea540bfe7982
r2dev2 <ronak.badhe@gmail.com>
2021-05-29 18:35:20 -0700

    can add macros

--- src/components/options/TranslatorMode.svelte
+++ src/components/options/TranslatorMode.svelte
@@ -1,10 +1,34 @@
 <script>
+  import { Col, Row, Subheader, Button, Icon } from 'svelte-materialify/src';
+  import { mdiPlus } from '@mdi/js';
+  import { onMount } from 'svelte';
+
   import CustomMacro from './CustomMacro.svelte';
   import { macros } from '../../js/store.js';
 
-  macros.set([{ name: 'peko', expansion: 'pekora', enabled: true }]);
+  const emptyMacro = { name: '', expansion: '', enabled: true };
+
+  const cleanUpMacros =
+    () => $macros = $macros.filter(m => m.name + m.expansion);
+
+  const createNewMacro =
+    () => [cleanUpMacros(), $macros = [...$macros, emptyMacro]];
+
+  onMount(cleanUpMacros);
 </script>
 
+<Row>
+  <Col>
+    <Subheader>Macros</Subheader>
+  </Col>
+  <Col style="padding-right: 2px;">
+    <Subheader style="float: right; padding-right: 0px">
+      <Button icon on:click={createNewMacro}>
+        <Icon path={mdiPlus} />
+      </Button>
+    </Subheader>
+  </Col>
+</Row>
 {#each $macros as macro, id}
   <CustomMacro {...macro} {id} />
 {/each}
