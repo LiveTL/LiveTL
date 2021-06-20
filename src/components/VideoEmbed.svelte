e3aad7130aab32b575d1804d465bb85f867b0df6
r2dev2 <ronak.badhe@gmail.com>
2021-06-19 23:03:11 -0700

    Fix #234

--- src/components/VideoEmbed.svelte
+++ src/components/VideoEmbed.svelte
@@ -1,5 +1,7 @@
 <script>
   import YouTubeIframeLoader from 'youtube-iframe';
+  import { videoSide } from '../js/store.js';
+  import { VideoSide } from '../js/constants.js';
 
   export let videoId;
   YouTubeIframeLoader.load(YT => {
@@ -29,8 +31,15 @@
   <link rel="icon" href={faviconURL} />
 </svelte:head>
 
-<div class="wrapper">
+<div class="wrapper" class:left-video={$videoSide == VideoSide.LEFT}>
   <div id="player" />
 </div>
 
-<style src="../css/iframe.css"></style>
+<!--<style src="../css/iframe.css"></style>-->
+
+<style src="../css/iframe.css">
+  /* Add 4px cause otherwise, there is visible grey for some reason */
+  .left-video {
+    width: calc(100% - var(--bar) + 4px);
+  }
+</style>
