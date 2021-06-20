afdde1c001770ab0ce94309a891c04e52164dd46
r2dev2 <ronak.badhe@gmail.com>
2021-06-20 14:32:20 -0700

    can kinda get mchad tls for a room

--- src/js/mchad.js
+++ src/js/mchad.js
@@ -1,4 +1,5 @@
 import { MCHAD } from './constants.js';
+import { derived, readable } from 'svelte/store';
 
 export async function getRooms(videoID) {
   return {
@@ -7,4 +8,20 @@ export async function getRooms(videoID) {
   };
 }
 
-window.getRooms = getRooms;
+export const streamRoom = room => readable(null, set => {
+  const source = new EventSource(`${MCHAD}/Listener?room=${room}`);
+  
+  source.onmessage = event => {
+    set(JSON.parse(event.data))
+  }
+  
+  return function stop() {
+    source.close();
+  }
+});
+
+export const getRoomTranslations = room => derived(streamRoom(room), (data, set) => {
+  if (data?.flag == 'insert') {
+    set(data.Stext);
+  }
+});
