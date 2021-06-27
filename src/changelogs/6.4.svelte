d3e06104427b680f96dadc7481360910bebc8aea
Kento Nishi <kento24gs@outlook.com>
2021-06-27 13:06:45 -0700

    bump extension version

--- src/changelogs/6.4.svelte
+++ src/changelogs/6.4.svelte
@@ -9,7 +9,8 @@
     <div>
       We have some exciting news to share with you regarding an all-new Android
       app, translation exports, external dedicated translator sources, and beta
-      features!
+      features! The post will be made public several hours after this update
+      goes public.
       <br />
       <a href="https://livetl.app/en/news/latest" target="_blank">
         <img
@@ -18,6 +19,14 @@
           alt="post"
         />Click here to see the post!</a
       >
+    </div>
+  </ExpansionPanel>
+  <ExpansionPanel>
+    <span slot="header">HyperChat improvements</span>
+    <div>
+      HyperChat now supports initial chat message data support! What this means
+      is that you no longer have to wait for the chat to load messages after
+      opening a stream.
     </div>
   </ExpansionPanel>
 </ExpansionPanels>
