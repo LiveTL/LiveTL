48162638edbff3c460ec4354266a2a64083623a8
r2dev2 <ronak.badhe@gmail.com>
2021-07-13 15:05:28 -0700

    vod ytc and mchad embed tests

--- e2e/test_ext.py
+++ e2e/test_ext.py
@@ -24,6 +24,9 @@ configure(
 
 
 chilled_cow = "https://www.youtube.com/watch?v=5qap5aO4i9A"
+peko_kiara = "https://www.youtube.com/watch?v=c747jYku6Eo"
+mio_phas = "https://www.youtube.com/watch?v=h7F4XJh41uo"
+phas = "Phasmophobia"
 
 
 @run_on(all_)
@@ -45,7 +48,7 @@ def test_injection(web):
 
 
 @run_on(all_)
-def test_open_embed(web):
+def test_embed_open(web):
     open_embed(web)
     *_, greeting = web.find_elements_by_css_selector("h2")
     assert greeting.text.strip() == "Welcome to LiveTL!"
@@ -58,14 +61,68 @@ def test_embed_resize(web):
     _, embed_window = web.find_elements_by_css_selector(".message-display-wrapper")
     handle, = web.find_elements_by_css_selector(".ui-resizable-handle")
 
+    # Move the embed handlebar down by 20px
     previous_height = embed_window.size["height"]
     ActionChains(web).drag_and_drop_by_offset(handle, 0, 20).perform()
     new_height = embed_window.size["height"]
-    assert abs(previous_height - new_height - 20) <= 2
+    assert abs(previous_height - new_height - 20) <= 2, "Moving the handlebar didn't resize the embed frame"
+
+    # Check if the embed resize is persistant
+    open_embed(web)
+    _, embed_window = web.find_elements_by_css_selector(".message-display-wrapper")
+    assert abs(embed_window.size["height"] - new_height) <= 2, "Embed resize is not persistent"
 
 
-def open_embed(web):
-    web.get(chilled_cow)
+@run_on(all_)
+def test_embed_mchad_vod_tls(web):
+    open_embed(web, mio_phas)
+    switch_to_youtube_parent_frame(web)
+    play_video(web)
+    wait_for_ads(web, phas)
+
+    # Go to 2/10 completion of the video and back
+    body, = web.find_elements_by_css_selector("body")
+    # body.click()
+    body.send_keys("2")
+    time.sleep(5)
+    body.send_keys("0")
+
+    switch_to_embed_frame(web)
+    web.save_screenshot("mio-mchad-tls.png")
+    tl, info = web.find_elements_by_css_selector(".message-display > .message > span")
+    assert "Mio, this time we're veteran" in tl.text, "Incorrect tl"
+    assert "MonMon TL" in info.text, "Incorrect author of tl"
+    assert "Mchad TL" in info.text, "MCHAD badge not found"
+    assert "(00:00:43)" in info.text, "Incorrect tl timestamp"
+
+
+@run_on(all_)
+def test_embed_ytc_vod_tls(web):
+    open_embed(web, peko_kiara)
+    switch_to_youtube_parent_frame(web)
+    play_video(web)
+    wait_for_ads(web, "PEKORA")
+
+    # Go to 3/10 completion of video where there is translation of
+    # tl: You sound so cool just now
+    # author: KFC
+    # timestamp: 23:51
+    body, = web.find_elements_by_css_selector("body")
+    # body.click()
+    body.send_keys("3")
+    time.sleep(5)
+
+    switch_to_embed_frame(web)
+    web.save_screenshot("kiara-tls.png")
+    tl, info = web.find_elements_by_css_selector(".message-display > .message > span")
+    assert "You sound so cool just now" in tl.text, "Incorrect tl"
+    assert "KFC" in info.text, "Incorrect author"
+    assert "Mchad TL" not in info.text, "MCHAD badge incorrectly displayed"
+    assert "(23:51)" in info.text, "Incorrect tl timestamp"
+
+
+def open_embed(web, site=chilled_cow):
+    web.get(site)
     time.sleep(5)
 
     web.switch_to.frame(web.find_elements_by_css_selector("#chatframe")[0])
@@ -77,6 +134,33 @@ def open_embed(web):
     # switch to embed frame
     web.switch_to.frame(web.find_elements_by_css_selector("iframe")[0])
     close_update_dialogue(web)
+
+
+def wait_for_ads(web, expected_part_of_title):
+    # title_el, = web.find_elements_by_css_selector(".ytp-title")
+    # while expected_part_of_title not in title_el.text:
+    while ">Ad " in web.page_source:
+        for skip in web.find_elements_by_css_selector(".ytp-ad-skip-button"):
+            with suppress(Exception):
+                skip.click()
+                return
+        time.sleep(1)
+
+
+def switch_to_youtube_parent_frame(web):
+    while not web.find_elements_by_css_selector("video"):
+        web.switch_to.parent_frame()
+
+
+def switch_to_embed_frame(web):
+    switch_to_youtube_parent_frame(web)
+    web.switch_to.frame(web.find_elements_by_css_selector("#chatframe")[0])
+    web.switch_to.frame(web.find_elements_by_css_selector("iframe")[0])
+
+
+def play_video(web):
+    with suppress(Exception):
+        web.find_elements_by_css_selector(".ytp-large-play-button")[0].click()
 
 
 def close_update_dialogue(web):
