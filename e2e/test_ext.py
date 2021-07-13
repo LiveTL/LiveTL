d7c634b750cd6dec880652f7400a6236f34689d2
r2dev2 <ronak.badhe@gmail.com>
2021-07-12 23:37:28 -0700

    fixed test cases and added more

--- e2e/test_ext.py
+++ e2e/test_ext.py
@@ -1,6 +1,9 @@
 import time
 import os
+from contextlib import suppress
 from pathlib import Path
+
+from selenium.webdriver.common.action_chains import ActionChains
 
 from autoparaselenium import configure, run_on, all_, Extension
 
@@ -28,6 +31,8 @@ def test_injection(web):
     web.get(chilled_cow)
     time.sleep(5)
 
+    web.switch_to.frame(web.find_elements_by_css_selector("#chatframe")[0])
+
     # LiveTL Buttons
     open_button, popout_button, embed_button = web.find_elements_by_css_selector(".livetlButtonWrapper > span")
     assert open_button.text.strip() == "Open LiveTL"
@@ -35,7 +40,45 @@ def test_injection(web):
     assert embed_button.text.strip() == "Embed TLs"
 
     # Hyperchat Buttons
-    ytc_frame, *_ = web.find_elements_by_css_selector("iframe")
-    web.switch_to.frame(ytc_frame)
-    hc_button = web.find_elements_by_css_selector(".toggleButtonContainer")
-    assert hc_button.get_attribute("data-tooltip") == "Enable HyperChat"
+    hc_button, = web.find_elements_by_css_selector(".toggleButtonContainer")
+    assert hc_button.get_attribute("data-tooltip") == "Disable HyperChat"
+
+
+@run_on(all_)
+def test_open_embed(web):
+    open_embed(web)
+    *_, greeting = web.find_elements_by_css_selector("h2")
+    assert greeting.text.strip() == "Welcome to LiveTL!"
+
+
+@run_on(all_)
+def test_embed_resize(web):
+    open_embed(web)
+
+    _, embed_window = web.find_elements_by_css_selector(".message-display-wrapper")
+    handle, = web.find_elements_by_css_selector(".ui-resizable-handle")
+
+    previous_height = embed_window.size["height"]
+    ActionChains(web).drag_and_drop_by_offset(handle, 0, 20).perform()
+    new_height = embed_window.size["height"]
+    assert abs(previous_height - new_height - 20) <= 1e-2
+
+
+def open_embed(web):
+    web.get(chilled_cow)
+    time.sleep(5)
+
+    web.switch_to.frame(web.find_elements_by_css_selector("#chatframe")[0])
+
+    *_, embed_button = web.find_elements_by_css_selector(".livetlButtonWrapper > span")
+    embed_button.click()
+    time.sleep(5)
+
+    # switch to embed frame
+    web.switch_to.frame(web.find_elements_by_css_selector("iframe")[0])
+    close_update_dialogue(web)
+
+
+def close_update_dialogue(web):
+    with suppress(Exception):
+        web.find_elements_by_css_selector('.s-dialog button.blue')[0].click()
