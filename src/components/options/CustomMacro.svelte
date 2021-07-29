9a98eb8f474248cc0fcf4f1b075e6b87bf324912
r2dev2 <ronak.badhe@gmail.com>
2021-07-29 15:36:30 -0700

    can delete macro

--- src/components/options/CustomMacro.svelte
+++ src/components/options/CustomMacro.svelte
@@ -1,5 +1,6 @@
 <script>
-  import { Row, Col, Checkbox, TextField } from 'svelte-materialify/src';
+  import { Button, Icon, Row, Col, Checkbox, TextField } from 'svelte-materialify/src';
+  import { mdiDelete } from '@mdi/js';
   import { macros } from '../../js/store.js';
 
   export let name;
@@ -12,6 +13,10 @@
   const saveValues = newMacro => {
     $macros = $macros.map((m, i) => i == id ? newMacro : m);
   };
+
+  const deleteMacro = () => {
+    $macros = $macros.filter((_, i) => i != id);
+  }
 
   $: saveValues({ name, expansion, enabled });
   $: if (field) {
@@ -29,4 +34,11 @@
     <TextField dense bind:value={expansion}>Expansion</TextField>
   </Col>
   <Checkbox bind:checked={enabled} />
+  <!--
+    @Kento, @ChrRubin, can you see whether width should also be set to 3.5em?
+    Idk which looks better
+  -->
+  <Button icon class="red-text" style="height: 3.5em;" on:click={deleteMacro}>
+    <Icon path={mdiDelete} />
+  </Button>
 </Row>
