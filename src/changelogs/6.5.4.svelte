63ddcc30e4585c7752d3df349c0befd0840afdfd
Kento Nishi <kento24gs@outlook.com>
2021-08-02 10:54:38 -0700

    updated v6.5.4 logs

--- src/changelogs/6.5.4.svelte
+++ src/changelogs/6.5.4.svelte
@@ -1,6 +1,6 @@
 <script>
   import { ExpansionPanel, ExpansionPanels } from 'svelte-materialify';
-  let value = [0];
+  let value = [];
 </script>
 
 <ExpansionPanels bind:value>
@@ -9,7 +9,23 @@
     <div>
       MChad users are now handled as individual users, just like translators in
       the YouTube chat! No longer do you need to keep seeing Korean TLers when
-      trying to get EN translations.
+      trying to get EN translations. There is also now a duplicate message
+      checker to prevent messages from MChad users showing up twice.
+      Additionally, we have addressed several issues with MChad stopping
+      mid-stream.
+    </div>
+  </ExpansionPanel>
+  <ExpansionPanel>
+    <span slot="header">Translator mode is here!</span>
+    <div>
+      Translators can now make use of our all-new auto-prefixing and macro
+      system. No longer do you have to type [en] before every message! You can
+      enable translator mode in the settings menu under the "Chat" tab.
+      <img
+        src="./img/TLMode.png"
+        style="width: 100%; height: auto;"
+        alt="screenshot"
+      />
     </div>
   </ExpansionPanel>
   <ExpansionPanel>
