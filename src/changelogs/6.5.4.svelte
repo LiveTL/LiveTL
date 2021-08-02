5d447dd7e741cbdc351d56ddc8f0aff9d3cb2aad
Kento Nishi <kento24gs@outlook.com>
2021-08-02 03:37:42 -0700

    v6.5.4

---
+++ src/changelogs/6.5.4.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { ExpansionPanel, ExpansionPanels } from 'svelte-materialify';
+  let value = [0];
+</script>
+
+<ExpansionPanels bind:value>
+  <ExpansionPanel>
+    <span slot="header">MChad TLer blacklisted</span>
+    <div>
+      MChad users are now handled as individual users, just like translators in
+      the YouTube chat! No longer do you need to keep seeing Korean TLers when
+      trying to get EN translations.
+    </div>
+  </ExpansionPanel>
+</ExpansionPanels>
--- src/changelogs/versionMap.js
+++ src/changelogs/versionMap.js
@@ -23,7 +23,8 @@ const changelogSequence = [
   '6.4.4',
   '6.4.5',
   '6.5',
-  '6.5.3'
+  '6.5.3',
+  '6.5.4'
 ];
 // eslint-disable-next-line no-unused-vars
 export default version => {
