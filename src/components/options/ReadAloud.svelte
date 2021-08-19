3be3520c0de345999a03bb92af8f4487606a5360
Kento Nishi <kento24gs@outlook.com>
2021-08-18 21:25:22 -0700

    finally a working dropdown

--- src/components/options/ReadAloud.svelte
+++ src/components/options/ReadAloud.svelte
@@ -10,20 +10,24 @@
   import SliderOption from '../options/Slider.svelte';
   import Dropdown from '../options/Dropdown.svelte';
   import { writable } from 'svelte/store';
-  import { onMount, onDestroy } from 'svelte';
 
   const store = writable('0');
   $: selectableLanguages = $voiceNames.map((n, i) => ({
       name: n,
       value: i.toString()
     }));
-  $: speechVoiceName.set($voiceNames[$store]);
   $: store.set(selectableLanguages.find(l => l.name == $speechVoiceName)?.value || '0');
+  $: speechVoiceName.set($voiceNames[$store]);
 </script>
 
 <CheckOption name="Read-aloud mode" store={doSpeechSynth} />
 {#if $doSpeechSynth}
   <SliderOption name="Speech volume" store={speechVolume} min={0} max={1} />
   <SliderOption name="Speech speed" store={speechSpeed} min={0.5} max={2} />
-  <Dropdown name="Speech synthesis voice" label={$speechVoiceName} {store} items={selectableLanguages} />
+  <Dropdown
+    name="Speech synthesis voice"
+    label={$speechVoiceName}
+    {store}
+    items={selectableLanguages}
+  />
 {/if}
