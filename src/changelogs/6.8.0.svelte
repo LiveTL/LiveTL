fafb01e73e4d2d3f0c3e809143927109927d9e54
Kento Nishi <kento24gs@outlook.com>
2021-08-26 16:08:18 -0700

    changelog for v6.8

---
+++ src/changelogs/6.8.0.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { ExpansionPanel, ExpansionPanels } from 'svelte-materialify';
+  let value = [0];
+</script>
+
+<ExpansionPanels bind:value>
+  <ExpansionPanel>
+    <span slot="header">No more unavailable videos!</span>
+    <div>
+      Previously, YouTube and/or streamers blocked third-party sites from
+      opening some streams. This prevented users from watching membership
+      streams and certain channels in the full-page LiveTL viewer. We've come up
+      with a workaround that forces videos to play anyway! No longer do you need
+      to resort to LiveTL's embedded mode to watch restricted streams.
+    </div>
+  </ExpansionPanel>
+</ExpansionPanels>
--- src/changelogs/versionMap.js
+++ src/changelogs/versionMap.js
@@ -30,7 +30,7 @@ const changelogSequence = [
   '6.6.2',
   '6.6.3',
   '6.7.0',
-  '6.7.2',
+  '6.8.0',
 ];
 // eslint-disable-next-line no-unused-vars
 export default version => {
