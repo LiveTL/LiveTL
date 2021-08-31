f65053beca2670bfba0fee9de9c21d8a1359088c
Kento Nishi <kento24gs@outlook.com>
2021-08-30 19:59:00 -0700

    new changelog to de-emphasize video loading

--- src/changelogs/6.8.0.svelte
+++ src/changelogs/6.8.0.svelte
@@ -1,17 +1,25 @@
 <script>
   import { ExpansionPanel, ExpansionPanels } from 'svelte-materialify';
-  let value = [0];
+  let value = [];
 </script>
 
 <ExpansionPanels bind:value>
   <ExpansionPanel>
-    <span slot="header">No more unavailable videos!</span>
+    <span slot="header">Video loading fixes</span>
     <div>
-      Previously, YouTube and/or streamers blocked third-party sites from
-      opening some streams. This prevented users from watching membership
-      streams and certain channels in the full-page LiveTL viewer. We've come up
-      with a workaround that forces videos to play anyway! No longer do you need
-      to resort to LiveTL's embedded mode to watch restricted streams.
+      Previously, YouTube and/or streamers could block third-party sites from
+      opening some videos. This prevented users from watching streams and
+      certain channels in the full-page LiveTL viewer. We've come up with a
+      sort-of-functional workaround that forces videos to play anyway! Some
+      videos that were unavailable before can now play (with occasional hiccups)
+      in the full-page LiveTL viewer, not just in embedded or popout mode.
+    </div>
+  </ExpansionPanel>
+  <ExpansionPanel>
+    <span slot="header">Translator mode macro fixes</span>
+    <div>
+      The macro feature in translator mode was partially broken in some corner
+      cases. These issues have been fixed.
     </div>
   </ExpansionPanel>
 </ExpansionPanels>
