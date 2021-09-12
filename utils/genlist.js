83548554053c7a6cc65c289179871611ea4ba6f3
r2dev2 <ronak.badhe@gmail.com>
2021-09-11 18:06:53 -0700

    clean up genlist code

--- utils/genlist.js
+++ utils/genlist.js
@@ -1,15 +1,8 @@
-const { spawn } = require('child_process');
 const fs = require('fs');
 const request = require('request');
 
-const cmds = [
-  'curl -o src/plugins/opencollective.json https://opencollective.com/livetl/members/all.json',
-  'curl -o src/plugins/gh.json https://api.github.com/repos/LiveTL/LiveTL/contributors',
-  'sed -i \':a;N;$!ba;s/\\n//g\' src/plugins/gh.json'
-];
-
 const keepAttrs = (...attrs) => obj => Object.fromEntries(Object.entries(obj)
-  .filter(([key, val]) => attrs.includes(key))
+  .filter(([key, _val]) => attrs.includes(key))
 );
 
 const jsonToArr = (...keys) => obj => keys
@@ -19,20 +12,22 @@ const req = request.defaults({
   headers: { 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0' }
 });
 
-req('https://opencollective.com/livetl/members/all.json', { json: true }, (err, _res, body) => {
+const downloadJson = (url, fp, keys) => req(url, { json: true }, (err, _res, body) => {
   if (err) return console.error(err);
-  const keys = ['totalAmountDonated', 'profile', 'role', 'name'];
-  fs.writeFileSync('./src/plugins/opencollective.json', JSON.stringify({
+  fs.writeFileSync(fp, JSON.stringify({
     keys,
     users: body.map(keepAttrs(...keys)).map(jsonToArr(...keys))
   }));
 });
 
-req('https://api.github.com/repos/LiveTL/LiveTL/contributors', { json: true }, (err, _res, body) => {
-  if (err) return console.error(err);
-  const keys = ['login', 'type'];
-  fs.writeFileSync('./src/plugins/gh.json', JSON.stringify({
-    keys,
-    users: body.map(keepAttrs(...keys)).map(jsonToArr(...keys))
-  }));
-});
+downloadJson(
+  'https://opencollective.com/livetl/members/all.json',
+  './src/plugins/opencollective.json',
+  ['totalAmountDonated', 'profile', 'role', 'name']
+);
+
+downloadJson(
+  'https://api.github.com/repos/LiveTL/LiveTL/contributors',
+  './src/plugins/gh.json',
+  ['login', 'type']
+);
