dc41e8287710e8f1717a9ebe8a4c9850d6f4686e
r2dev2 <ronak.badhe@gmail.com>
2021-09-21 22:25:36 -0700

    add icons and improve taishi shill in changelog

--- src/changelogs/6.8.0.svelte
+++ src/changelogs/6.8.0.svelte
@@ -1,11 +1,32 @@
 <script>
-  import { ExpansionPanel, ExpansionPanels } from 'svelte-materialify';
+  import { ExpansionPanel, ExpansionPanels, Icon } from 'svelte-materialify/src';
+  import { mdiBug, mdiBullhorn, mdiReload, mdiRocketLaunch } from '@mdi/js';
   let value = [];
 </script>
 
 <ExpansionPanels bind:value>
   <ExpansionPanel>
-    <span slot="header">Auto-launch mode!</span>
+    <span slot="header"><Icon path={mdiBullhorn} /> SHOUT-OUT: Translator Kamishiro Taishi's talk show</span>
+    <div>
+      You probably know Kamishiro Taishi, a legendary live translator in the
+      Vtuber community. He will be hosting a new talk show called TLTalk
+      that aims to introduce people involved with translating —
+      such as translators and software developers — with a focus on those tied
+      to Hololive and Vtubers in general. <a
+        href="https://www.youtube.com/watch?v=cGcBHb6OcDc"
+        target="_blank"
+        style="font-size: 1.1rem;"
+        >Make sure to tune in on YouTube and subscribe!
+        <img
+          src="./img/TLTalk.jpg"
+          style="width: 100%; height: auto;"
+          alt="TLTalk"
+        /></a
+      >
+    </div>
+  </ExpansionPanel>
+  <ExpansionPanel>
+    <span slot="header"><Icon path={mdiRocketLaunch} /> Auto-launch mode!</span>
     <div>
       You can now configure LiveTL to automatically launch in a specific mode!
       If you have the option enabled, LiveTL will either redirect, embed, or
@@ -14,7 +35,7 @@
     </div>
   </ExpansionPanel>
   <ExpansionPanel>
-    <span slot="header">Video loading fixes</span>
+    <span slot="header"><Icon path={mdiReload} /> Video loading fixes</span>
     <div>
       Previously, YouTube and/or streamers could block third-party sites from
       opening some videos. This prevented users from watching streams and
@@ -25,29 +46,10 @@
     </div>
   </ExpansionPanel>
   <ExpansionPanel>
-    <span slot="header">Translator mode macro fixes</span>
+    <span slot="header"><Icon path={mdiBug} /> Translator mode macro fixes</span>
     <div>
       The macro feature in translator mode was partially broken in some corner
       cases. These issues have been fixed.
-    </div>
-  </ExpansionPanel>
-  <ExpansionPanel>
-    <span slot="header">Translator Kamishiro Taishi's talk show</span>
-    <div>
-      You probably know Kamishiro Taishi, a legendary live translator in the
-      Vtuber community. He will be hosting a new show called TLTalk! TLTalk is a
-      live talkshow that aims to introduce people involved with translating —
-      such as translators and software developers — with a focus on those tied
-      to Hololive and Vtubers in general. <a
-        href="https://www.youtube.com/watch?v=cGcBHb6OcDc"
-        target="_blank"
-        >Make sure to tune in on YouTube!
-        <img
-          src="./img/TLTalk.jpg"
-          style="width: 100%; height: auto;"
-          alt="TLTalk"
-        /></a
-      >
     </div>
   </ExpansionPanel>
 </ExpansionPanels>
