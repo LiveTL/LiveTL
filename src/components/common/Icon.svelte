70ed1803dac08e4bfadc27294aa2e901815a2982
ChrRubin <20059164+ChrRubin@users.noreply.github.com>
2021-10-25 14:37:34 +0800

    Use submod Icon.svelte

--- src/components/common/Icon.svelte
+++ src/components/common/Icon.svelte
@@ -1,20 +1,17 @@
 <script lang="ts">
+  import Icon from '../../submodules/chat/src/components/common/Icon.svelte';
   export let block = true;
   export let small = false;
   export let xs = false;
 
-  $: classes = 'material-icons icon select-none duration-200 ease-in ' +
-    ($$props.class ? $$props.class : '');
+  $: fontSize = small ? '1' : xs ? '0.75' : '1.25';
 </script>
 
-<i
-  aria-hidden="true"
-  class={classes}
+<Icon
   on:click
-  class:block
-  class:text-base={small}
-  class:text-xs={xs}
-  style="font-size: 1.25em; {$$props.style ? $$props.style : ''}"
+  class={$$props.class ?? ''}
+  style="font-size: {fontSize}em; {$$props.style ?? ''}"
+  {block}
 >
   <slot />
-</i>
+</Icon>
