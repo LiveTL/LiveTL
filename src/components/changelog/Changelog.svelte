9b69e8ba6547f91ab0ce28439e84b4600b5abeeb
ChrRubin <20059164+ChrRubin@users.noreply.github.com>
2021-10-25 18:23:46 +0800

    Add more v7 changelogs

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -1,5 +1,5 @@
 <script>
-  export const version = '7.0.2';
+  export const version = '7.0.3';
   import ExpandingCard from '../common/ExpandingCard.svelte';
 </script>
 
@@ -7,4 +7,24 @@
   We re-wrote the entire interface! This time, we used a smaller library that
   should make everything more lightweight. This will also enable the LiveTL
   component to be mounted in third-party sites in the future (stay tuned!).
+</ExpandingCard>
+<ExpandingCard title="Introducing HyperChat v2!" icon="chat">
+  HyperChat has also been rewritten with the same library as LiveTL v7, making
+  it lighter and even faster than before! This new update also brings features
+  such as pinned messages, Super Stickers, Membership Items and Membership
+  Milestone Chats into HyperChat!
+</ExpandingCard>
+<ExpandingCard title="New features and bug fixes" icon="new_releases">
+  <ul class="list-disc text-base p-2 list-inside">
+    <li>
+      A new button has been added to Embed Mode that will open the stream in
+      LiveTL Mode.
+      <img
+        class="py-1"
+        src="https://cdn.discordapp.com/attachments/786713928264319026/902138793501552660/unknown.png"
+        alt="Expand LiveTL"
+      />
+    </li>
+    <li>Embed Mode will no longer open with vertical chat split.</li>
+  </ul>
 </ExpandingCard>
