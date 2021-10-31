2978526cf00701fd62f04a4c0d7e1087d1f91300
Kento Nishi <kento24gs@outlook.com>
2021-10-31 10:32:44 -0700

    cleanup before release

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -1,18 +1,19 @@
 <script>
   export const version = '7.0.4';
   import ExpandingCard from '../common/ExpandingCard.svelte';
+  import ExpandLiveTL from './assets/ExpandLiveTL.png';
 </script>
 
 <ExpandingCard title="All-new interface... again!" icon="group">
   We re-wrote the entire interface! This time, we used a smaller library that
-  should make everything more lightweight. This will also enable the LiveTL
-  component to be mounted in third-party sites in the future (stay tuned!).
+  should make everything more lightweight. Loading times and overall performance
+  should be improved!
 </ExpandingCard>
 <ExpandingCard title="Introducing HyperChat v2!" icon="chat">
   HyperChat has also been rewritten with the same library as LiveTL v7, making
   it lighter and even faster than before! This new update also brings features
-  such as pinned messages, Super Stickers, Membership Items and Membership
-  Milestone Chats into HyperChat!
+  such as pinned messages, super stickers, membership items, and membership
+  milestone chats into HyperChat!
 </ExpandingCard>
 <ExpandingCard title="New feature in Embed Mode" icon="code">
   A new button has been added to Embed Mode that will open the stream in LiveTL
@@ -30,6 +31,15 @@
 </ExpandingCard>
 <ExpandingCard title="Bug fixes" icon="new_releases">
   <ul class="list-disc text-base p-2 list-inside">
+    <li>
+      A new button has been added to Embed Mode that will open the stream in
+      LiveTL Mode.
+      <img
+        class="py-1"
+        src={ExpandLiveTL}
+        alt="Expand LiveTL"
+      />
+    </li>
     <li>Embed Mode will no longer open with vertical chat split.</li>
     <li>Fix auto launch mode not working in Firefox.</li>
   </ul>
