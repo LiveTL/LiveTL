04a79e6abe49cee81268b8fdb2e3923c84b6cdb7
r2dev2 <ronak.badhe@gmail.com>
2021-10-30 22:32:08 -0700

    formatting

--- src/__tests__/js/mchad.js
+++ src/__tests__/js/mchad.js
@@ -2,13 +2,13 @@
 import { getRoomTagLanguageCode } from '../../js/mchad.js';
 
 it.each([
-  [ 'en', 'en' ],
-  [ 'eng', 'en' ],
-  [ 'Français', 'fr' ],
-  [ '日本語', 'jp' ],
-  [ 'es -> en', 'en' ],
-  [ '', null ],
-  [ 'lalala lalala', null ]
+  ['en', 'en'],
+  ['eng', 'en'],
+  ['Français', 'fr'],
+  ['日本語', 'jp'],
+  ['es -> en', 'en'],
+  ['', null],
+  ['lalala lalala', null]
 ])('getRoomTagLanguageCode("%s") == %s', (tag, expected) => {
   expect(getRoomTagLanguageCode(tag)).toEqual(expected);
 });
