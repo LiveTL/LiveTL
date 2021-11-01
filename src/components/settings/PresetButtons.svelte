9a96317f6f41ffbe6ad3d9f6fee06e6156c2e796
Erox1 <InfinityWaffles@outlook.com>
2021-11-01 10:19:52 -0700

    Fixed Suggestions
    Changed the language tag to typescript and switched the types to typescript.
    
    Removed the unnecessary double binding of the active variable
    
    Removed the .get() functions and replaced them with $'s
    
    And added translator mode settings, key-bind's and the window size to the presets as well as slightly changed the formatting of the code.

--- src/components/settings/PresetButtons.svelte
+++ src/components/settings/PresetButtons.svelte
@@ -1,56 +1,76 @@
-<script>
+<script lang="ts">
   import {
     captionDuration,
     captionFontSize,
+    captionWidth,
+    captionLeft,
+    captionTop,
     videoSideSetting,
     chatZoom,
     livetlFontSize,
     textDirection,
     chatSplit,
-    autoVertical,
-    autoLaunchMode,
     activePreset,
-    presets
+    presets,
+    videoPanelSize,
+    chatSize,
+    keyboardShortcuts,
+    doTranslatorMode,
+    doAutoPrefix,
+    autoPrefixTag,
+    macroTrigger,
+    macros
   } from '../../js/store';
   import { importStores } from '../../js/storage';
   import Button from 'smelte/src/components/Button';
 
-  $: active = $activePreset;
+  let active = $activePreset;
   $: activePreset.set(active);
 
-  /**
-  * @param {number} number
-  */
-  async function updatePreset(number) {
+  async function updatePreset(number: number) {
     if (number !== active) {
-      importStores(JSON.stringify(presets.get()[number - 1]));
+      importStores(JSON.stringify($presets[number - 1]));
       active = number;
       return;
     }
 
-    const presetData = presets.get();
+    const presetData = $presets;
     presetData[number - 1] = {
-      captionDuration: captionDuration.get(),
-      captionFontSize: captionFontSize.get(),
-      videoSideSetting: videoSideSetting.get(),
-      chatZoom: chatZoom.get(),
-      livetlFontSize: livetlFontSize.get(),
-      textDirection: textDirection.get(),
-      chatSplit: chatSplit.get(),
-      autoVertical: autoVertical.get(),
-      autoLaunchMode: autoLaunchMode.get()
+      captionDuration: $captionDuration,
+      captionFontSize: $captionFontSize,
+      captionWidth: $captionWidth,
+      captionLeft: $captionLeft,
+      captionTop: $captionTop,
+
+      videoSideSetting: $videoSideSetting,
+      videoPanelSize: $videoPanelSize,
+
+      chatZoom: $chatZoom,
+      chatSize: $chatSize,
+      chatSplit: $chatSplit,
+
+      textDirection: $textDirection,
+      livetlFontSize: $livetlFontSize,
+
+      keyboardShortcuts: $keyboardShortcuts,
+
+      doTranslatorMode: $doTranslatorMode,
+      doAutoPrefix: $doAutoPrefix,
+      autoPrefixTag: $autoPrefixTag,
+      macroTrigger: $macroTrigger,
+      macros: $macros
     };
     presets.set(presetData);
   }
 </script>
 
 {#each [1, 2] as i}
-    <div class="flex gap-2 py-1">
-      <Button add="flex-1" on:click={() => { updatePreset(2 * i - 1); }} color="dark" light={active === 2 * i - 1}>
-        {active !== 2 * i - 1 ? `Preset ${2 * i - 1}` : `Save Preset ${2 * i - 1}`}
-      </Button>
-      <Button add="flex-1" on:click={() => { updatePreset(2 * i); }} color="dark" light={active === 2 * i}>
-        {active !== 2 * i ? `Preset ${2 * i}` : `Save Preset ${2 * i}`}
-      </Button>
-    </div>
+  <div class="flex gap-2 py-1">
+    <Button add="flex-1" on:click={() => { updatePreset(2 * i - 1); }} color="dark" light={active === 2 * i - 1}>
+      {active !== 2 * i - 1 ? `Preset ${2 * i - 1}` : `Save Preset ${2 * i - 1}`}
+    </Button>
+    <Button add="flex-1" on:click={() => { updatePreset(2 * i); }} color="dark" light={active === 2 * i}>
+      {active !== 2 * i ? `Preset ${2 * i}` : `Save Preset ${2 * i}`}
+    </Button>
+  </div>
 {/each}
