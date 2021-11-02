94fc63cd514b286e11c5feec3970aa53c2d30092
Erox1 <InfinityWaffles@outlook.com>
2021-11-02 14:41:36 -0700

    Clead Up Preset Button Code
    Cleaned up the preset button code by moving the button code into it's own script and cleaning up the way the variables are generated.

---
+++ src/components/settings/PresetButton.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import Button from 'smelte/src/components/Button';
+  
+  export let deletePreset: (number: number) => {};
+  export let updatePreset: (number: number) => {};
+
+  export let isDeleting: boolean = false;
+  export let active: number = 0;
+  export let number: number = 0;
+</script>
+
+<Button 
+  add="flex-1" 
+  on:click={() => { 
+    isDeleting ? 
+      deletePreset(number) : 
+      updatePreset(number); 
+  }} 
+  color="dark" 
+  light={ active === number && !isDeleting }
+>
+  {
+    isDeleting ? 
+      `Delete Preset ${number}` : 
+      active !== number ? 
+        `Preset ${number}` : 
+        `Save Preset ${number}`
+  }
+</Button>
