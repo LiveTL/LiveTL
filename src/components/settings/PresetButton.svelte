015a16b646a4ed62c726f458173cfe920d28073a
Erox1 <InfinityWaffles@outlook.com>
2021-11-04 10:17:14 -0700

    Switch To Event Listeners
    Switched the preset buttons to use event listeners instead of explicit functions.

--- src/components/settings/PresetButton.svelte
+++ src/components/settings/PresetButton.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
   import Button from 'smelte/src/components/Button';
-  
-  export let deletePreset: (number: number) => {};
-  export let updatePreset: (number: number) => {};
+  import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+  function updatePreset(presetNumber: number) {
+    dispatch("updatePreset",
+      { presetNumber }
+    )
+  }
+  function deletePreset(presetNumber: number) {
+    dispatch("deletePreset",
+      { presetNumber }
+    )
+  }
 
   export let isDeleting: boolean = false;
   export let activeNumber: number = 0;
