09b561a5f520484566be155faf1373dd39382005
Erox1 <InfinityWaffles@outlook.com>
2021-11-06 12:32:13 -0700

    Added Renaming Presets
    Added the ability to rename presets through a new green button with the edit icon.

--- src/components/settings/PresetButton.svelte
+++ src/components/settings/PresetButton.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import Button from 'smelte/src/components/Button';
+  import Icon from 'smelte/src/components/Icon';
+  import TextField from '../common/TextField.svelte';
   import { createEventDispatcher } from 'svelte';
 
 const dispatch = createEventDispatcher();
@@ -14,26 +16,56 @@ const dispatch = createEventDispatcher();
       { presetNumber }
     );
   }
+  function renamePreset(presetNumber: number, name: string) {
+    dispatch('renamePreset',
+      { 
+        presetNumber, 
+        name 
+      }
+    );
+  }
 
   export let isDeleting: boolean = false;
   export let activeNumber: number = 0;
   export let prefabNumber: number = 0;
+  export let name: string = `Preset ${prefabNumber}`;
+
+  let isEditing: boolean = false;
+  const editingInputClass: string = "button text-center text-white rounded col-span-4 bg-gray-300 dark:bg-dark-400 uppercase text-sm font-medium relative";
 </script>
 
-<Button 
-  on:click={() => {
-    isDeleting
-      ? deletePreset(prefabNumber)
-      : updatePreset(prefabNumber);
-  }} 
-  color="dark" 
-  light={ activeNumber === prefabNumber && !isDeleting }
->
-  {
-    isDeleting
-      ? `Delete Preset ${prefabNumber}`
-      : activeNumber !== prefabNumber
-        ? `Preset ${prefabNumber}`
-        : `Save Preset ${prefabNumber}`
-  }
-</Button>
+<div class="grid grid-cols-5 gap-2">
+  {#if !isEditing}
+    <Button 
+      on:click={() => {
+        isDeleting
+          ? deletePreset(prefabNumber)
+          : updatePreset(prefabNumber);
+      }} 
+      color="dark" 
+      light={ activeNumber === prefabNumber && !isDeleting }
+      add="col-span-4"
+    >
+      {
+        isDeleting
+          ? `Delete ${name}`
+          : activeNumber !== prefabNumber
+            ? name
+            : `Save ${name}`
+      }
+    </Button>
+  {:else}
+    <input 
+      class={editingInputClass} 
+      bind:value={name}
+      on:change={() => { renamePreset(prefabNumber, name); }}
+    />
+  {/if}
+  <Button 
+    add="flex content-center justify-center"
+    color="success"
+    on:click={() => (isEditing = !isEditing)}
+  >
+    <Icon>{"edit"}</Icon>
+  </Button>
+</div>
--- src/components/settings/PresetButtons.svelte
+++ src/components/settings/PresetButtons.svelte
@@ -10,6 +10,8 @@
   let activeNumber = $activePreset;
   $: activePreset.set(activeNumber);
 
+  let presetsData = $presets;
+
   async function updatePreset(event: CustomEvent<{ presetNumber: number }>) {
     const presetNumber = event.detail.presetNumber;
     if (presetNumber !== activeNumber) {
@@ -18,45 +20,53 @@
       return;
     }
 
-    const presetsData = $presets;
     presetsData[presetNumber - 1] = Object.fromEntries(
-      presetStores.map((store) => [store.name, store.get()])
+      presetStores.map((store) => [store.name, store.get()]).concat([["name", presetsData[presetNumber - 1]["name"]]])
     );
     presets.set(presetsData);
   }
 
   async function deletePreset(event: CustomEvent<{ presetNumber: number }>) {
     const presetNumber = event.detail.presetNumber;
-    const presetsData = $presets;
     presetsData.splice(presetNumber - 1, 1);
     presets.set(presetsData);
     presetAmount -= 1;
   }
 
   async function addPreset() {
-    const presetsData = $presets;
     presetsData.push(
       Object.fromEntries(
-        presetStores.map((store) => [store.name, store.defaultValue])
+        presetStores.map((store) => [store.name, store.defaultValue]).concat([["name", 'Preset']])
       )
     );
     presets.set(presetsData);
     presetAmount += 1;
   }
+
+  async function renamePreset(event: CustomEvent<{ presetNumber: number, name: string }>) {
+    const updatedPresetName = event.detail.name;
+    const presetNumber = event.detail.presetNumber;
+
+    presetsData[presetNumber - 1]['name'] = updatedPresetName
+    presets.set(presetsData);
+  }
 </script>
 
-<div class="grid grid-cols-2 gap-2 py-1">
+
+<div class="grid grid-cols-1 gap-2 py-1">
   {#each Array(presetAmount) as _, i}
     <PresetButton
       {activeNumber}
       {isDeleting}
       on:deletePreset={deletePreset}
       on:updatePreset={updatePreset}
+      on:renamePreset={renamePreset}
+      name={presetsData[i]['name']}
       prefabNumber={i + 1}
     />
   {/each}
 </div>
-<div class="grid grid-cols-2 gap-2">
+<div class="grid grid-cols-2 gap-2 py-1">
   <Button color="blue" on:click={addPreset}>Add Preset</Button>
   <Button
     color="error"
