b56c144d82f1e4bb39c64e798aa722c5a81287b8
Kento Nishi <kento24gs@outlook.com>
2021-12-02 12:06:50 -0800

    include layout presets info

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -1,5 +1,6 @@
 <script>
   import ExpandingCard from '../common/ExpandingCard.svelte';
+  import LayoutPresets from './assets/LayoutPresets.png';
 </script>
 
 <ExpandingCard title="One-Year Anniversary!" icon="event_repeat" expanded>
@@ -11,7 +12,12 @@
   <a
     href="https://www.reddit.com/r/Hololive/comments/r7btmn/oneyear_anniversary_appreciation_post_see_comments/"
     target="_blank"
-    class="text-blue-400 underline"
-    >View Post</a
+    class="text-blue-400 underline">View Post</a
   >
+</ExpandingCard>
+<ExpandingCard title="Layout presets" icon="windows">
+  You can now save layout preferences as "presets" in the settings menu! With
+  just a few clicks, you can jump between your favorite UI layout
+  configurations.
+  <img class="pt-2" src={LayoutPresets} alt="LiveTL Layout Presets" />
 </ExpandingCard>
