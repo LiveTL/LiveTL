19c73f71a943a86b30018ca14583400423837dd4
ChrRubin <20059164+ChrRubin@users.noreply.github.com>
2021-12-11 02:36:14 +0800

    Fix base64, add refresh function

--- src/ts/auth.ts
+++ src/ts/auth.ts
@@ -1,20 +1,23 @@
 import sha256 from 'crypto-js/sha256';
+import base64 from 'crypto-js/enc-base64';
+import utf8 from 'crypto-js/enc-utf8';
 import { config } from './auth-config';
 import axios from 'axios';
-import type { AxiosResponse } from 'axios';
+import type WordArray from 'crypto-js/lib-typedarrays';
 
 // https://gist.github.com/alexdiliberto/39a4ad0453310d0a69ce
-function randomBytes(size: number): Buffer {
+function randomBytes(size: number): string {
   const QUOTA = 65536;
   const a = new Uint8Array(size);
   for (let i = 0; i < size; i += QUOTA) {
     crypto.getRandomValues(a.subarray(i, i + Math.min(QUOTA, size - i)));
   }
-  return Buffer.from(a);
+  return a.join('');
 }
 
-function base64URLEncode(str: any): string {
-  return str.toString('base64')
+function base64URLEncode(str: string | WordArray): string {
+  const wordArray = typeof str === 'string' ? utf8.parse(str) : str;
+  return base64.stringify(wordArray)
     .replace(/\+/g, '-')
     .replace(/\//g, '_')
     .replace(/=/g, '');
@@ -28,9 +31,19 @@ interface TokenResult {
   expires_in: number;
 }
 
+function isTokenResult(obj: any): obj is TokenResult {
+  return (
+    obj.access_token !== undefined &&
+    obj.refresh_token !== undefined &&
+    obj.id_token !== undefined &&
+    obj.token_type !== undefined &&
+    obj.expires_in !== undefined
+  );
+}
+
 export async function authenticate(): Promise<TokenResult> {
   const verifier = base64URLEncode(randomBytes(32));
-  const challenge = base64URLEncode(sha256(verifier));
+  const challenge = base64URLEncode(sha256(verifier).toString());
 
   const authOptions = {
     response_type: 'code',
@@ -50,11 +63,11 @@ export async function authenticate(): Promise<TokenResult> {
     }, resolve);
   });
 
-  if (resultUrl == null || resultUrl === '') throw new Error('Authorization failed. No result URL.');
+  if (resultUrl == null || resultUrl === '') throw new Error('Auth failed. No result URL.');
 
   const code = new URL(resultUrl).searchParams.get('code');
 
-  if (code == null) throw new Error('Authorization failed. No code.');
+  if (code == null) throw new Error('Auth failed. No code.');
 
   const body = JSON.stringify({
     grant_type: 'authorization_code',
@@ -64,13 +77,33 @@ export async function authenticate(): Promise<TokenResult> {
     client_id: config.clientId
   });
 
-  const result: AxiosResponse<TokenResult, any> = await axios.post(`https://${config.domain}/oauth/token`, body, {
+  const result = await axios.post(`https://${config.domain}/oauth/token`, body, {
     headers: {
       'Content-Type': 'application/json'
     }
   });
 
-  if (result.status !== 200) throw new Error('Authorization failed. Post failed.');
+  if (result.status !== 200) throw new Error('Auth failed. Post failed.');
+  if (!isTokenResult(result.data)) throw new Error('Auth failed. Invalid result.');
+
+  return result.data;
+}
+
+export async function refresh(refreshToken: string): Promise<TokenResult> {
+  const body = JSON.stringify({
+    grant_type: 'refresh_token',
+    refresh_token: refreshToken,
+    client_id: config.clientId
+  });
+
+  const result = await axios.post(`https://${config.domain}/oauth/token`, body, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (result.status !== 200) throw new Error('Refresh failed. Post failed.');
+  if (!isTokenResult(result.data)) throw new Error('Refresh failed. Invalid result.');
 
   return result.data;
 }
