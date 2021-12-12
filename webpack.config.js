7781f4890f012ec3a48f647b9cb0123fc78263b1
Kento Nishi <kento24gs@outlook.com>
2021-12-11 17:32:58 -0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -296,7 +296,7 @@ module.exports = (env, options) => {
     config.plugins.push(new webpack.HotModuleReplacementPlugin());
     config.devServer = {
       host: 'localhost',
-      port: 3000,
+      port: parseInt(process.env.PORT || '3000'),
       disableHostCheck: true,
       hot: true,
       contentBase: path.join(__dirname, '../build'),
