3f16eaf90a1e9de45d314f1d68a2ffd6d704394c
Kento Nishi <kento24gs@outlook.com>
2022-01-12 10:31:33 -0800

    changelog

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -2,19 +2,10 @@
   import ExpandingCard from '../common/ExpandingCard.svelte';
 </script>
 
-<ExpandingCard title="Verified user filter" icon="verified_user">
-  You can now filter chat messages from verified users (enabled by default)!
-</ExpandingCard>
-<ExpandingCard title="Feature hints" icon="info">
-  When some recommended features are available but not enabled (ex. translator
-  spotlighting), a nice animation will be shown to let you know! Our hope is
-  that this will reveal features built into LiveTL that you may not have known
-  about.
-</ExpandingCard>
-<ExpandingCard title="Chat layout options" icon="chat">
-  You can now flip the placement of the chat and the LiveTL panel! Head over to
-  the settings menu to fiddle with your layout.
-</ExpandingCard>
-<ExpandingCard title="Internal improvements" icon="video_settings">
-  Our internal interactions with YouTube's video player are now more secure.
+<ExpandingCard title="HyperChat" icon="verified_user">
+  HyperChat now stores settings in extension storage! This update is a precursor
+  to the next major update, which will include a HyperChat-specific settings
+  menu. We also improved chat message timing mechanics, so messages should be a
+  little less delayed. PS. We heard that Kiara (my personal oshi) got HyperChat
+  -- thank you, and we hope you like it!
 </ExpandingCard>
