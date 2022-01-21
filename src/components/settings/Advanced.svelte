4305d75ca5fec1f3bc3ecbebff6517826c3efefe
Kento Nishi <kento24gs@outlook.com>
2022-01-21 11:06:46 -0800

    changed some text

--- src/components/settings/Advanced.svelte
+++ src/components/settings/Advanced.svelte
@@ -3,9 +3,12 @@
   import About from './About.svelte';
   import Card from '../common/Card.svelte';
   import Checkbox from '../common/CheckboxStore.svelte';
+  import NonstoreCheckbox from '../common/Checkbox.svelte';
 </script>
 
-<Card title="Additional sites support" icon="web">
+<Card title="Additional sites" icon="web">
+  Enable LiveTL on:
+  <NonstoreCheckbox label="YouTube" checked disabled />
   <Checkbox name="Twitch" store={twitchEnabled} />
 </Card>
 <Card title="About" icon="info">
