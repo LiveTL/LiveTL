e01398e33c6c6e0af1294874f42fab79e7464159
Kento Nishi <kento24gs@outlook.com>
2022-01-21 18:13:25 -0800

    demo image

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -1,8 +1,10 @@
 <script>
   import ExpandingCard from '../common/ExpandingCard.svelte';
+  import twitch from './twitch.png';
 </script>
 
 <ExpandingCard title="Twitch Support!" icon="live_tv" expanded>
   LiveTL now works on Twitch! You can filter chat messages, view MChad TLs, and
   more, just like on YouTube.
+  <img class="pt-2" src={twitch} alt="LiveTL on Twitch" />
 </ExpandingCard>
