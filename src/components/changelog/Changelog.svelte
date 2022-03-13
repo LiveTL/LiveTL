91697902aa56bb7d7aa79d6e1e11f6c54eef7503
Kento Nishi <kento24gs@outlook.com>
2022-03-12 18:34:52 -0800

    v8.2.0

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -2,12 +2,27 @@
   import ExpandingCard from '../common/ExpandingCard.svelte';
 </script>
 
-<ExpandingCard title="Automatic chat message translation in HyperChat!" icon="chat">
-  Introducing automatic chat message translation (via Google Translate)!
-  You can enable the feature in the HyperChat settings menu.
-  <i>Special thanks to tenchou for the idea!</i>
+<ExpandingCard title="New HyperChat features!" icon="chat">
+  <ul class="list-disc list-inside">
+    <strong>On today's KFP menu:</strong>
+    <li class="ml-3.5">
+      Show only messages from channel members
+    </li>
+    <li class="ml-3.5">
+      Hide emoji-only spam messages or hide all emotes
+    </li>
+  </ul>
+  <ul class="list-disc list-inside">
+    <strong>What's cooking in the usual room:</strong>
+    <li class="ml-3.5">
+      Fixing message translation language selection
+    </li>
+    <li class="ml-3.5">
+      Reducing live chat message delays
+    </li>
+    <li class="ml-3.5">
+      Fixing colored emoji rendering issues
+    </li>
+  </ul>  
 </ExpandingCard>
 
-<ExpandingCard title="Internal improvements" icon="code">
-  This update adds performance improvements.
-</ExpandingCard>
