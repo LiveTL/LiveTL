4504ad539c91f3a86bb388b62133cf8a27c8245b
Kento Nishi <kento24gs@outlook.com>
2022-03-20 21:27:55 -0700

    v8.2.1

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -1,28 +1,9 @@
 <script>
   import ExpandingCard from '../common/ExpandingCard.svelte';
+  import HCChangelogs from '../../submodules/chat/src/components/changelog/Changelog.svelte';
 </script>
 
-<ExpandingCard title="New HyperChat features!" icon="chat" expanded>
-  <ul class="list-disc list-inside">
-    <strong>On today's KFP menu:</strong>
-    <li class="ml-3.5">
-      Show only messages from channel members
-    </li>
-    <li class="ml-3.5">
-      Hide emoji-only spam messages or hide all emotes
-    </li>
-  </ul>
-  <ul class="list-disc list-inside">
-    <strong>What's cooking in the usual room:</strong>
-    <li class="ml-3.5">
-      Fixing message translation language selection
-    </li>
-    <li class="ml-3.5">
-      Reducing live chat message delays
-    </li>
-    <li class="ml-3.5">
-      Fixing colored emoji rendering issues
-    </li>
-  </ul>
+<ExpandingCard title="HyperChat Fixes and Improvements" icon="chat" expanded>
+  <HCChangelogs />
 </ExpandingCard>
 
