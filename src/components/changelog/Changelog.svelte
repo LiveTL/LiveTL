ac05c7d1a01816949e27aef3dea436544a30b87f
Kento Nishi <kento24gs@outlook.com>
2022-04-18 12:47:20 -0700

    capitalization

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -6,6 +6,6 @@
 <ExpandingCard title="Fixes for read-aloud mode" icon="record_voice_over">
   The read-aloud mode occasionally read messages twice. The issue has been fixed!
 </ExpandingCard>
-<ExpandingCard title="HyperChat Fixes and Improvements" icon="chat">
+<ExpandingCard title="HyperChat fixes and improvements" icon="chat">
   <HCChangelogs />
 </ExpandingCard>
