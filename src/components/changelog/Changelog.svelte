82a65a0c65d8d32049d300506be9a7ff955043bf
Kento Nishi <kento24gs@outlook.com>
2022-05-05 15:20:59 -0700

    v8.2.6

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -3,9 +3,6 @@
   import HCChangelogs from '../../submodules/chat/src/components/changelog/Changelog.svelte';
 </script>
 
-<ExpandingCard title="Fixes for read-aloud mode" icon="record_voice_over">
-  The read-aloud mode occasionally read messages twice. The issue has been fixed!
-</ExpandingCard>
 <ExpandingCard title="HyperChat fixes and improvements" icon="chat">
   <HCChangelogs />
 </ExpandingCard>
