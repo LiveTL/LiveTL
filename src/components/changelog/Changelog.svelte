c6f1ed52f13e679b4bef496029f0d4c66ac3f8fe
Kento Nishi <kento24gs@outlook.com>
2022-05-30 16:18:51 -0700

    v8.2.9

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -1,8 +1,16 @@
 <script>
   import ExpandingCard from '../common/ExpandingCard.svelte';
-  import HCChangelogs from '../../submodules/chat/src/components/changelog/Changelog.svelte';
+  // import HCChangelogs from '../../submodules/chat/src/components/changelog/Changelog.svelte';
 </script>
 
-<ExpandingCard title="HyperChat fixes and improvements" icon="chat" expanded>
-  <HCChangelogs />
+<ExpandingCard title="TLdex Integration" icon="chat" expanded>
+  In addition to the YouTube chat, LiveTL now hooks into TLdex (Holodex) to provide
+  translations more quickly and more reliably! TLdex fully replaces all MChad functionality
+  as the service has been taken offline. Thank you to the Holodex team for making this possible,
+  and we wish MonMon Mnemonics (MChad dev) the best of luck with his PhD! o7
 </ExpandingCard>
+<ExpandingCard title="Translation Export Fixes" icon="chat" expanded>
+  Previously, there were visual bugs when exporting translations as a screenshot or text file.
+  The issues have been fixed!
+</ExpandingCard>
+
