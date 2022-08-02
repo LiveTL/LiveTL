bdf3030604c26680e2291b19b4c4c144807e32ac
Kento Nishi <kento24gs@outlook.com>
2022-08-02 13:26:35 -0700

    tltalk

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -1,8 +1,13 @@
 <script>
   import ExpandingCard from '../common/ExpandingCard.svelte';
-  import HCChangelogs from '../../submodules/chat/src/components/changelog/Changelog.svelte';
+  const d = new Date(1659837600 * 1000);
 </script>
 
-<ExpandingCard title="HyperChat Improvements" icon="chat">
-  <HCChangelogs />
+<ExpandingCard title="TLTalk!" icon="chat" expanded>
+  I (Kento, developer of HyperChat, LiveTL, etc.) will be going on Kamishiro Taishi's
+  "TLTalk" show! Tune in on {d.toLocaleDateString()} at {d.toLocaleTimeString()} to learn
+  more about who I am, how LiveTL came to be, and what we're working on.
+  <a href="https://youtu.be/m0D9hsI2wuc" target="_blank" class="underline">
+    Bookmark the stream here.
+  </a>
 </ExpandingCard>
