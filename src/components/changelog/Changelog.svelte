f60e9814b4adc69d888a74f600c305ca5e7e31c9
Kento Nishi <kento24gs@outlook.com>
2022-09-11 13:52:27 -0400

    v8.3.6

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -1,8 +1,12 @@
 <script>
   import ExpandingCard from '../common/ExpandingCard.svelte';
-  import HCChangelogs from '../../submodules/chat/src/components/changelog/Changelog.svelte';
+  // import HCChangelogs from '../../submodules/chat/src/components/changelog/Changelog.svelte';
 </script>
 
-<ExpandingCard title="HyperChat Improvements" icon="chat">
+<!-- <ExpandingCard title="HyperChat Improvements" icon="chat">
   <HCChangelogs />
+</ExpandingCard> -->
+
+<ExpandingCard title="TLdex Support for Twitch VODs" icon="chat">
+  Translations available on TLdex are now accessible on Twitch stream replays (VODs)!
 </ExpandingCard>
