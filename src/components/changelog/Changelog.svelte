3f0197c6c26307859334e9909f0ca1f3c9b9e0f1
Kento Nishi <kento24gs@outlook.com>
2023-07-28 11:30:10 -0400

    changelogs

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -1,24 +1,8 @@
 <script>
   import ExpandingCard from '../common/ExpandingCard.svelte';
-  // import HCChangelogs from '../../submodules/chat/src/components/changelog/Changelog.svelte';
-  // const trailer = chrome.runtime.getURL(
-  //   'hyperchat/trailer.png'
-  // );
+  import HCChangelogs from '../../submodules/chat/src/components/changelog/Changelog.svelte';
 </script>
 
-<ExpandingCard title="YouTube Studio Bug Fixes" icon="chat">
-  Some bugs with LiveTL and HyperChat in YouTube Studio have been fixed.
+<ExpandingCard title="HyperChat Improvements" icon="chat">
+  <HCChangelogs />
 </ExpandingCard>
-<ExpandingCard title="TLdex Fixes" icon="chat">
-  TLdex connectivity issues have been fixed.
-</ExpandingCard>
-
-<!-- <ExpandingCard title="Twitch and TLdex Fixes" icon="chat" expanded> -->
-  <!-- <div style="width: 100%; display: flex; justify-content: center; margin: 8px 0px;">
-    <a href="https://www.youtube.com/watch?v=FU3YWQy_Rfs" target="_blank">
-      <img src={trailer} style="width: 100%; aspect-ratio: calc(16 / 9);" alt="HyperChat Trailer" class="rounded-sm" />
-    </a>
-  </div> -->
-  <!-- Due to changes to YouTube and Twitch, some users were experiencing
-  issues with Twitch and TLdex. These issues have been fixed. 
-</ExpandingCard> -->
