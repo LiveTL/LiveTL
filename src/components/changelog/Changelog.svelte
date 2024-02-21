2db000609c715732db9acd333e7f5a30ae0b41f2
Kento Nishi <kento24gs@outlook.com>
2024-02-21 18:07:36 -0500

    hc changelogs

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -1,13 +1,13 @@
 <script>
   import ExpandingCard from '../common/ExpandingCard.svelte';
-  // import HCChangelogs from '../../submodules/chat/src/components/changelog/Changelog.svelte';
+  import HCChangelogs from '../../submodules/chat/src/components/changelog/Changelog.svelte';
 </script>
 
-<!-- <ExpandingCard title="HyperChat Improvements" icon="chat">
+<ExpandingCard title="HyperChat Fixes" icon="chat">
   <HCChangelogs />
-</ExpandingCard> -->
-<ExpandingCard title="Translator Mode Fixes" icon="chat">
+</ExpandingCard>
+<!-- <ExpandingCard title="Translator Mode Fixes" icon="chat">
   Translator mode has been upgraded to be compatible with recent changes
   to YouTube chat. The new version should be more resilient to additional
   changes on YouTube's end going forward.
-</ExpandingCard>
+</ExpandingCard> -->
