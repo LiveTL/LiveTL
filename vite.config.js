5cfc1572a01e36c35dc404fe59fd639c0cbffcff
r2dev2 <ronak.badhe@gmail.com>
2024-03-01 05:06:07 -0800

    chrome does not like underscore in file name

--- vite.config.js
+++ vite.config.js
@@ -41,12 +41,27 @@ for (const entry of entryPoints) {
   fs.writeFileSync(`src/${entry.name}`, htmlEntry);
 }
 
+// chrome does not like _ in filename
+// see: https://github.com/rollup/rollup/issues/4772#issuecomment-1366727437
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F_]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+function sanitizeFileName(name) {
+  const match = DRIVE_LETTER_REGEX.exec(name);
+  const driveLetter = match ? match[0] : '';
+  return driveLetter + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '+');
+}
+
 export default defineConfig({
   root: 'src',
   build: {
     outDir: path.resolve(__dirname, 'build'),
     emptyOutDir: true,
-    minify: process.env.MINIFY !== 'false'
+    minify: process.env.MINIFY !== 'false',
+    rollupOptions: {
+      output: {
+        sanitizeFileName
+      }
+    }
   },
   resolve: {
     alias: {
