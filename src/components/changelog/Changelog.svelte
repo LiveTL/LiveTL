47f4d7dbd5c0dcb3fd3093412a601fc9d098ed9d
Kento Nishi <kento24gs@outlook.com>
2024-07-07 13:29:13 -0400

    bump hc

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -3,18 +3,7 @@
   import HCChangelogs from '../../submodules/chat/src/components/changelog/Changelog.svelte';
 </script>
 
-<ExpandingCard title="Twitch Fixes" icon="live_tv">
-  This version restores LiveTL's functionality on Twitch (which
-  was broken in the previous version).
-</ExpandingCard>
-
-<ExpandingCard title="Manifest v3 Update" icon="extension">
-  After two years of struggles, the LiveTL Manifest v3 upgrade
-  is now live for Chromium-based browsers! This update ensures
-  that the extension will prevail through Google's scheduled purge
-  of older extensions based on Manifest v2.
-</ExpandingCard>
-
-<ExpandingCard title="HyperChat v3 Update" icon="chat">
-  <HCChangelogs />
+<ExpandingCard title="Message Timestamp Fixes" icon="chat">
+  <!-- <HCChangelogs /> -->
+  This update implements some message timing fixes.
 </ExpandingCard>
