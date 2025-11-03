909031a13e5acdfd323c0885b32fb9091922bf57
r2dev2 <ronak.badhe@gmail.com>
2025-11-03 02:56:19 -0800

    add youtu.be as a referer to all youtube.com/embed/* urls

--- src/js/pages/background.js
+++ src/js/pages/background.js
@@ -20,3 +20,17 @@ chrome.webRequest.onHeadersReceived.addListener(
       '<all_urls>'
     ]
   }, ['blocking', 'responseHeaders']);
+
+
+// Fixes #478 - embed view does not work without a proper referer
+// youtu.be can be anything just not empty
+// stick to youtu.be to make youtube think this is just a redirect
+browser.webRequest.onBeforeSendHeaders.addListener(
+  details => {
+    const headers = details.requestHeaders;
+    headers.push({ name: 'Referer', value: 'https://youtu.be' });
+    return { requestHeaders: headers };
+  },
+  { urls: ['*://*.youtube.com/embed/*'] },
+  ['blocking', 'requestHeaders']
+);
