e4ca95b1cfaf4f9ffd40d5eda2695bfbcc8b89ed
Kento Nishi <kento24gs@outlook.com>
2025-11-05 19:59:52 -0500

    changelog

--- src/components/changelog/Changelog.svelte
+++ src/components/changelog/Changelog.svelte
@@ -1,8 +1,8 @@
 <script>
   import ExpandingCard from '../common/ExpandingCard.svelte';
-  import HCChangelogs from '../../submodules/chat/src/components/changelog/Changelog.svelte';
+  // import HCChangelogs from '../../submodules/chat/src/components/changelog/Changelog.svelte';
 </script>
 
-<ExpandingCard title="New HyperChat features" icon="chat">
-  <HCChangelogs />
+<ExpandingCard title="Bug fixes" icon="chat">
+  Fixed embeds in "Open in LiveTL" view.
 </ExpandingCard>
